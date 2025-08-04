239ec2c534748c2c2cd89f435ceb0fc52863c084
kangminju <yessuk5499@gmail.com>
2025-08-04 23:57:58 +0900

    색상 파일 생성

---
+++ app/src/main/java/com/example/danew_app/core/theme/Colors.kt
@@ -0,0 +1,27 @@
+package com.example.danew_app.core.theme
+
+import androidx.compose.ui.graphics.Color
+
+object ColorsLight {
+    val primaryColor = Color(0xFF0B942D)
+    val secondaryColor = Color(0xFFD1D2B0)
+    val lightGrayColor = Color(0xFFF6F6F6)
+    val darkGrayColor = Color(0xFF313131)
+    val grayColor = Color(0xFF676767)
+    val redColor = Color(0xFFE64856)
+    val blueColor = Color(0xFF3182F7)
+    val blackColor = Color(0xFF000000);
+    val whiteColor = Color(0xFFFFFFFF);
+}
+
+object ColorsDark {
+    val primaryColor = Color(0xFF0B942D)
+    val secondaryColor = Color(0xFF9BA18D)
+    val lightGrayColor = Color(0xFF2A2A2A)
+    val darkGrayColor = Color(0xFF121212)
+    val grayColor = Color(0xFF444444)
+    val redColor = Color(0xFFFF6F71)
+    val blueColor = Color(0xFF90C9F9)
+    val blackColor = Color(0xFF000000)
+    val whiteColor = Color(0xFFFFFFFF)
+}
