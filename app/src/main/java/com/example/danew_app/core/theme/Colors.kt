f5065d1eba26341fe6ff15692723aab5be51fcff
kangminju <yessuk5499@gmail.com>
2025-08-05 02:23:43 +0900

    색깔 수정

--- app/src/main/java/com/example/danew_app/core/theme/Colors.kt
+++ app/src/main/java/com/example/danew_app/core/theme/Colors.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 object ColorsLight {
     val primaryColor = Color(0xFF0B942D)
     val secondaryColor = Color(0xFFD1D2B0)
-    val lightGrayColor = Color(0xFFF6F6F6)
+    val lightGrayColor = Color(0xFFEAEAEA)
     val darkGrayColor = Color(0xFF313131)
     val grayColor = Color(0xFF676767)
     val redColor = Color(0xFFE64856)
