e3dc6998510927a9a2a27ca617da027d2cb5a3b9
kangminju <yessuk5499@gmail.com>
2025-08-05 18:28:29 +0900

    TopImageCard 생성

---
+++ app/src/main/java/com/example/danew_app/presentation/home/TopImageCard.kt
@@ -0,0 +1,35 @@
+package com.example.danew_app.presentation.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberAsyncImagePainter
+
+//현재 Top뉴스의 첫번째 카드
+@Composable
+fun TopImageCard(imageUrl: String, title: String) {
+    Column {
+        Image(
+            painter = rememberAsyncImagePainter(imageUrl),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+                .clip(RoundedCornerShape(8.dp)),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(title, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+    }
+}
