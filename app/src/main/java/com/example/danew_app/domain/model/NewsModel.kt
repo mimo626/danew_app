42554ffd3c5872144067df4aa5173302ea097a18
kangminju <yessuk5499@gmail.com>
2025-08-05 23:50:14 +0900

    NewsModel 구현

---
+++ app/src/main/java/com/example/danew_app/domain/model/NewsModel.kt
@@ -0,0 +1,12 @@
+package com.example.danew_app.domain.model
+
+data class NewsModel(
+    val id: String,
+    val title: String,
+    val description: String,
+    val imageUrl: String?,
+    val sourceName: String,
+    val pubDate: String,
+    val category: List<String>,
+    val link: String?
+)
