eec2dbea33d7127e1809d0d915de2500410bda5b
kangminju <yessuk5499@gmail.com>
2025-08-05 18:28:49 +0900

    NewsApi 생성

---
+++ app/src/main/java/com/example/danew_app/data/remote/NewsApi.kt
@@ -0,0 +1,14 @@
+package com.example.danew_app.data.remote
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+//Retrofit 인터페이스, API 요청의 명세를 정의
+interface NewsApi {
+    @GET("latest")
+    suspend fun fetchNewsByCategory(
+        @Query("country") country: String = "kr",
+        @Query("category") category: String,
+        @Query("apikey") apiKey: String = "pub_83780fb69a56440dbbeb6c4373fcb5ebb1535"
+    ): NewsResponse
+}
