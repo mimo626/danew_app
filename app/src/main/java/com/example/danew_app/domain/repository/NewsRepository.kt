efe267ccd81e6176e7e8e4b953080e6f2354badb
kangminju <yessuk5499@gmail.com>
2025-08-05 23:50:24 +0900

    NewsRepository 구현

---
+++ app/src/main/java/com/example/danew_app/domain/repository/NewsRepository.kt
@@ -0,0 +1,22 @@
+package com.example.danew_app.domain.repository
+
+import com.example.danew_app.data.repository.NewsRepositoryImpl
+import com.example.danew_app.domain.model.NewsModel
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+interface NewsRepository {
+    suspend fun getNewsByCategory(category: String): List<NewsModel>
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun bindNewsRepository(
+        impl: NewsRepositoryImpl
+    ): NewsRepository
+}
