054203563aaae0dd2982a9ecf90aad32afd85ba1
kangminju <yessuk5499@gmail.com>
2025-08-05 18:27:33 +0900

    메인 화면 위젯화

--- app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.danew.core.componet.BottomNavBar
 import com.example.danew.core.navigation.BottomNavGraph
-import com.example.danew_app.ui.theme.Danew_appTheme
-import com.example.myapp.ui.theme.MyAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
