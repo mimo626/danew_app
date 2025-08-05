fce8980345485e7af544369ed14b371490c7e105
kangminju <yessuk5499@gmail.com>
2025-08-05 23:50:29 +0900

    NewsRepositoryImpl 구현

--- app/src/main/java/com/example/danew_app/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/danew_app/data/repository/NewsRepositoryImpl.kt
@@ -1,4 +1,17 @@
 package com.example.danew_app.data.repository
 
-class NewsRepositoryImpl {
+import com.example.danew_app.data.mapper.toDomain
+import com.example.danew_app.data.remote.NewsApi
+import com.example.danew_app.domain.model.NewsModel
+import com.example.danew_app.domain.repository.NewsRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class NewsRepositoryImpl @Inject constructor(
+    private val api: NewsApi
+) : NewsRepository {
+    override suspend fun getNewsByCategory(category: String): List<NewsModel> {
+        return api.fetchNewsByCategory(category = category).results.map { it.toDomain() }
+    }
 }
