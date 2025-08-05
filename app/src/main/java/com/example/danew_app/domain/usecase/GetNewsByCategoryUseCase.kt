c04e2033645d3e2cc01c85d8d9764c1786a25b9b
kangminju <yessuk5499@gmail.com>
2025-08-05 23:51:23 +0900

    GetNewsByCategoryUseCase 구현

---
+++ app/src/main/java/com/example/danew_app/domain/usecase/GetNewsByCategoryUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.danew_app.domain.usecase
+
+import com.example.danew_app.domain.model.NewsModel
+import com.example.danew_app.domain.repository.NewsRepository
+import javax.inject.Inject
+
+class GetNewsByCategoryUseCase @Inject constructor(
+    private val repository: NewsRepository
+) {
+    suspend operator fun invoke(category: String): List<NewsModel> {
+        return repository.getNewsByCategory(category)
+    }
+}
