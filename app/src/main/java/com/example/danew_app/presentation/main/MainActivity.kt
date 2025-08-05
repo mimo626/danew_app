156090c6f965c7fb3e8b214c8b0a1a146c06cca2
kangminju <yessuk5499@gmail.com>
2025-08-05 23:51:43 +0900

    MainActivity 의존성 주입

--- app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.example.danew.core.componet.BottomNavBar
 import com.example.danew.core.navigation.BottomNavGraph
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
