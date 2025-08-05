f19ba3e77fe968d308084f485bf909773653402b
kangminju <yessuk5499@gmail.com>
2025-08-05 18:27:53 +0900

    NewsList 생성

---
+++ app/src/main/java/com/example/danew_app/presentation/home/NewList.kt
@@ -0,0 +1,25 @@
+package com.example.danew_app.presentation.home
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.danew.presentation.home.Article
+
+// 가로로 뉴스 리스트(작은 사진)
+@Composable
+fun NewsList(sectionTitle: String, articles: List<Article>) {
+    Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+        Text(sectionTitle, fontWeight = FontWeight.Bold, fontSize = 16.sp)
+        Spacer(modifier = Modifier.height(8.dp))
+        articles.forEach {
+            NewsItem(it)
+        }
+    }
+}
