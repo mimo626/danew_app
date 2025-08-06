28ee25846da76aa7fbe4e14e8dbeab9b31158872
kangminju <yessuk5499@gmail.com>
2025-08-06 17:28:34 +0900

    NewsCard 수정

--- app/src/main/java/com/example/danew_app/presentation/home/NewsCard.kt
+++ app/src/main/java/com/example/danew_app/presentation/home/NewsCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,13 +26,18 @@ fun NewsCard(newsModel: NewsModel) {
     Box(
         modifier = Modifier
             .height(100.dp)
-            .width(224.dp)
+            .width(200.dp)
             .padding(end = 16.dp)
             .background(ColorsLight.lightGrayColor, shape = RoundedCornerShape(4.dp)),
         contentAlignment = Alignment.CenterStart
     ) {
         Column (modifier = Modifier.padding(horizontal = 16.dp)){
-            Text(newsModel.title, maxLines = 2, overflow = TextOverflow.Ellipsis)
+            Text(newsModel.title, maxLines = 1, fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                overflow = TextOverflow.Ellipsis
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(newsModel.title, maxLines = 1, overflow = TextOverflow.Ellipsis)
             Spacer(modifier = Modifier.height(8.dp))
             // Todo 시간으로 계산
             Text(newsModel.pubDate, color = Color.Gray, fontSize = 12.sp)
