8c0c724fa7cba83e75ca3180917b2773472165f0
kangminju <yessuk5499@gmail.com>
2025-08-06 17:28:49 +0900

    TopImageCard 수정

--- app/src/main/java/com/example/danew_app/presentation/home/TopImageCard.kt
+++ app/src/main/java/com/example/danew_app/presentation/home/TopImageCard.kt
@@ -1,13 +1,20 @@
 package com.example.danew_app.presentation.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -15,21 +22,35 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
+import com.example.danew_app.core.theme.ColorsLight
 import com.example.danew_app.domain.model.NewsModel
 
 //현재 Top뉴스의 첫번째 카드
 @Composable
 fun TopImageCard(newsModel: NewsModel) {
     Column {
-        Image(
-            painter = rememberAsyncImagePainter(newsModel.imageUrl),
-            contentDescription = null,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(180.dp)
-                .clip(RoundedCornerShape(8.dp)),
-            contentScale = ContentScale.Crop
-        )
+        if(newsModel.imageUrl != null){
+            Image(
+                painter = rememberAsyncImagePainter(newsModel.imageUrl),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+        }else {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(180.dp)
+                    .background(ColorsLight.lightGrayColor, shape = RoundedCornerShape(8.dp)),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(Icons.Default.Warning, contentDescription = "썸네일")
+            }
+        }
+
         Spacer(modifier = Modifier.height(8.dp))
         Text(newsModel.title, fontWeight = FontWeight.Bold, fontSize = 16.sp)
     }
