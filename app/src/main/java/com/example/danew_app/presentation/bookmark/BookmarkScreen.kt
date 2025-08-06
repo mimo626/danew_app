6a72e742509224e389614f63496909dcb922af73
kangminju <yessuk5499@gmail.com>
2025-08-06 19:12:27 +0900

    북마크 화면 구현

--- app/src/main/java/com/example/danew_app/presentation/bookmark/BookmarkScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/bookmark/BookmarkScreen.kt
@@ -1,9 +1,40 @@
 package com.example.danew.presentation.bookmark
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.danew_app.core.gloabals.Globals
+import com.example.danew_app.core.theme.ColorsLight
+import com.example.danew_app.presentation.home.MainTopAppBar
+import com.example.danew_app.presentation.home.NewsItem
 
 @Composable
 fun BookmarkScreen() {
-    Text("북마크 화면")
+    Scaffold (
+        containerColor = ColorsLight.whiteColor,
+        topBar =  {
+            MainTopAppBar(
+                title = "북마크",
+                icon = Icons.Default.Notifications,
+                isHome = false)
+        }
+    ){
+            padding ->
+        Column(modifier = Modifier.fillMaxSize().padding(padding).padding(20.dp)) {
+            // 뉴스 리스트
+            LazyColumn {
+                items(Globals.dummyNewsList) { news ->
+                    NewsItem(newsModel = news)
+                }
+            }
+        }
+    }
 }
