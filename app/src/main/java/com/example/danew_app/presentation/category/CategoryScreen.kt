cf00e6a5d5446a0bc3f40bd9fb6cdf05773dc9be
kangminju <yessuk5499@gmail.com>
2025-08-06 17:27:59 +0900

    카테고리 화면 앱바 추가

--- app/src/main/java/com/example/danew_app/presentation/category/CategoryScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/category/CategoryScreen.kt
@@ -7,8 +7,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRowDefaults
@@ -30,6 +33,8 @@ import androidx.compose.runtime.getValue
 import com.example.danew_app.domain.model.NewsModel
 import com.example.danew_app.presentation.category.NewsCategory
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.danew_app.core.theme.ColorsLight
+import com.example.danew_app.presentation.home.MainTopAppBar
 
 val newsCategoryKr = NewsCategory.categoryKrToEn.keys.toList()
 
@@ -48,49 +53,62 @@ fun CategoryScreen(viewModel: NewsViewModel = hiltViewModel()) {
         viewModel.fetchNewsByCategory(enCategory)
     }
 
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
-        // 탭 UI
-        ScrollableTabRow(
-            selectedTabIndex = selectedTabIndex,
-            edgePadding = 0.dp,
-            indicator = { tabPositions ->
-                TabRowDefaults.Indicator(
-                    Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex])
-                )
+    Scaffold (
+        containerColor = ColorsLight.whiteColor,
+        topBar =  {
+            MainTopAppBar(
+            title = "카테고리",
+            icon = Icons.Default.Notifications,
+            isHome = false)
+        }
+    ){
+        padding ->
+        Column(modifier = Modifier.fillMaxSize().padding(padding).padding(20.dp)) {
+            // 탭 UI
+            ScrollableTabRow(
+                containerColor = ColorsLight.whiteColor,
+                selectedTabIndex = selectedTabIndex,
+                edgePadding = 0.dp,
+                indicator = { tabPositions ->
+                    TabRowDefaults.Indicator(
+                        Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex])
+                    )
+
+                }
+            ) {
+                newsCategoryKr.forEachIndexed { index, category ->
+                    Tab(
+                        selected = selectedTabIndex == index,
+                        onClick = { selectedTabIndex = index },
+                        text = {
+                            Text(
+                                category,
+                                color = if (selectedTabIndex == index) Color.Black else Color.Gray,
+                                fontWeight = if (selectedTabIndex == index) FontWeight.Bold else FontWeight.Normal
+                            )
+                        }
+                    )
+                }
             }
-        ) {
-            newsCategoryKr.forEachIndexed { index, category ->
-                Tab(
-                    selected = selectedTabIndex == index,
-                    onClick = { selectedTabIndex = index },
-                    text = {
-                        Text(
-                            category,
-                            color = if (selectedTabIndex == index) Color.Black else Color.Gray,
-                            fontWeight = if (selectedTabIndex == index) FontWeight.Bold else FontWeight.Normal
-                        )
-                    }
-                )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            // 로딩
+            if (isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
             }
-        }
 
-        Spacer(modifier = Modifier.height(12.dp))
+            // 에러 메시지
+            errorMessage?.let {
+                Text("오류: $it", color = Color.Red)
+            }
 
-        // 로딩
-        if (isLoading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-        }
-
-        // 에러 메시지
-        errorMessage?.let {
-            Text("오류: $it", color = Color.Red)
-        }
-
-        // 뉴스 리스트
-        LazyColumn {
-            items(newsList) { news ->
-                val category = newsCategoryKr[selectedTabIndex]
-                NewsItem(news = news, category = category)
+            // 뉴스 리스트
+            LazyColumn {
+                items(newsList) { news ->
+                    val category = newsCategoryKr[selectedTabIndex]
+                    NewsItem(news = news, category = category)
+                }
             }
         }
     }
@@ -100,7 +118,7 @@ fun CategoryScreen(viewModel: NewsViewModel = hiltViewModel()) {
 fun NewsItem(news: NewsModel, category: String) {
     Column(modifier = Modifier.padding(8.dp)) {
         Text(news.title, fontWeight = FontWeight.Bold)
-        Text(news.description, maxLines = 2, overflow = TextOverflow.Ellipsis)
+        Text(news.description, maxLines = 1, overflow = TextOverflow.Ellipsis)
         Text(news.pubDate, style = MaterialTheme.typography.bodySmall)
     }
 }
