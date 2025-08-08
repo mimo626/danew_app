d159d2c0bd1993fcb275e1e33a71c73af386e87b
kangminju <yessuk5499@gmail.com>
2025-08-08 20:17:47 +0900

    뉴스 메퍼 수정

--- app/src/main/java/com/example/danew_app/data/mapper/NewsMapper.kt
+++ app/src/main/java/com/example/danew_app/data/mapper/NewsMapper.kt
@@ -5,12 +5,12 @@ import com.example.danew_app.domain.model.NewsModel
 
 fun NewsEntity.toDomain(): NewsModel {
     return NewsModel(
-        id = this.articleId ?: "",
+        id = this.article_id ?: "",
         title = this.title ?: "제목 없음",
         description = this.description ?: "설명이 없습니다.",
-        imageUrl = this.imageUrl,
-        sourceName = this.sourceName ?: "알 수 없음",
-        pubDate = this.pubDate,
+        imageUrl = this.image_url,
+        sourceName = this.source_name ?: "알 수 없음",
+        pubDate = this.pub_date,
         category = this.category,
         link = this.link,
         creator = this.creator,
