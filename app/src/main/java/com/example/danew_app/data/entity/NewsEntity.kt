894a2a3dd98879da4875af5357e4246e0d51aa22
kangminju <yessuk5499@gmail.com>
2025-08-08 20:17:24 +0900

    NewsEntity 수정

--- app/src/main/java/com/example/danew_app/data/entity/NewsEntity.kt
+++ app/src/main/java/com/example/danew_app/data/entity/NewsEntity.kt
@@ -1,34 +1,31 @@
 package com.example.danew_app.data.entity
 
-import kotlinx.datetime.LocalDateTime
 import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
 
-@Serializable
 data class NewsEntity(
-    @SerialName("article_id") val articleId: String? = null,
+    @SerialName("article_id") val article_id: String? = null,
     val title: String? = null,
     val link: String? = null,
     val keywords: List<String> = emptyList(),
     val creator: List<String>? = emptyList(),
     val description: String? = null,
     val content: String? = null,
-    @SerialName("pub_date") val pubDate: String,  // ISO8601 형식 문자열
-    @SerialName("pub_date_tz") val pubDateTz: String? = null,
-    @SerialName("image_url") val imageUrl: String? = null,
-    @SerialName("video_url") val videoUrl: String? = null,
-    @SerialName("source_id") val sourceId: String? = null,
-    @SerialName("source_name") val sourceName: String? = null,
-    @SerialName("source_priority") val sourcePriority: Int,
-    @SerialName("source_url") val sourceUrl: String? = null,
-    @SerialName("source_icon") val sourceIcon: String? = null,
+    @SerialName("pub_date") val pub_date: String,  // ISO8601 형식 문자열
+    @SerialName("pub_date_tz") val pub_date_tz: String? = null,
+    @SerialName("image_url") val image_url: String? = null,
+    @SerialName("video_url") val video_url: String? = null,
+    @SerialName("source_id") val source_id: String? = null,
+    @SerialName("source_name") val source_name: String? = null,
+    @SerialName("source_priority") val source_priority: Int,
+    @SerialName("source_url") val source_url: String? = null,
+    @SerialName("source_icon") val source_icon: String? = null,
     val language: String? = null,
     val country: List<String> = emptyList(),
     val category: List<String>? = emptyList(),
     val sentiment: String? = null,
-    @SerialName("sentiment_stats") val sentimentStats: String? = null,
-    @SerialName("ai_tag") val aiTag: String? = null,
-    @SerialName("ai_region") val aiRegion: String? = null,
-    @SerialName("ai_org") val aiOrg: String? = null,
+    @SerialName("sentiment_stats") val sentiment_stats: String? = null,
+    @SerialName("ai_tag") val ai_tag: String? = null,
+    @SerialName("ai_region") val ai_region: String? = null,
+    @SerialName("ai_org") val ai_org: String? = null,
     val duplicate: Boolean
 )
