c706cbd042354a249dc7a4b25c9b9333cc9b4c8b
kangminju <yessuk5499@gmail.com>
2025-08-11 01:41:16 +0900

    안 쓰는 import 제거

--- app/src/main/java/com/example/danew_app/presentation/home/NewsDetailScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/home/NewsDetailScreen.kt
@@ -1,4 +1,3 @@
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -20,7 +19,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -31,7 +29,6 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
-import com.example.danew_app.domain.model.NewsModel
 import com.example.danew_app.presentation.category.NewsViewModel
 
 @Composable
