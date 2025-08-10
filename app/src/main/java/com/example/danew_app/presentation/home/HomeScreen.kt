a6d11d1e61b23a6caf746ac176a28ace72219989
kangminju <yessuk5499@gmail.com>
2025-08-11 01:37:12 +0900

    홈 화면에 navController 추가

--- app/src/main/java/com/example/danew_app/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/home/HomeScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.example.danew_app.core.gloabals.Globals
 import com.example.danew_app.domain.model.NewsModel
 import com.example.danew_app.presentation.home.MainTopAppBar
@@ -21,7 +22,7 @@ import com.example.danew_app.presentation.home.NowTopNews
 import com.example.danew_app.presentation.home.SearchBar
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(navController: NavHostController,) {
 
     Scaffold(
         topBar = {
@@ -43,9 +44,9 @@ fun HomeScreen() {
             Spacer(modifier = Modifier.height(24.dp))
             MainImageCard(Globals.dummyNewsList.get(0))
             Spacer(modifier = Modifier.height(32.dp))
-            NewsList("민주님을 위한 추천 뉴스", Globals.dummyNewsList)
+            NewsList("민주님을 위한 추천 뉴스", Globals.dummyNewsList, navController)
             Spacer(modifier = Modifier.height(16.dp))
-            NowTopNews("현재 TOP 경제 뉴스", Globals.dummyNewsList)
+            NowTopNews("현재 TOP 경제 뉴스", Globals.dummyNewsList, navController)
             Spacer(modifier = Modifier.height(28.dp))
         }
     }
