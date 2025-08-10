d53a6937609e26eb195cf0f6a5401c2f96311cd8
kangminju <yessuk5499@gmail.com>
2025-08-11 01:36:11 +0900

    뉴스 상세 화면 이동 추가

--- app/src/main/java/com/example/danew_app/core/navigation/BottomNavGraph.kt
+++ app/src/main/java/com/example/danew_app/core/navigation/BottomNavGraph.kt
@@ -1,26 +1,37 @@
 package com.example.danew.core.navigation
 
+import NewsDetailScreen
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
 import com.example.danew.presentation.bookmark.BookmarkScreen
 import com.example.danew.presentation.category.CategoryScreen
 import com.example.danew.presentation.diary.DiaryScreen
 import com.example.danew.presentation.home.HomeScreen
 import com.example.danew.presentation.profile.MyPageScreen
-import com.example.danew_app.presentation.home.NewsDetailScreen
+import com.example.danew_app.domain.model.NewsModel
+
+import com.google.gson.Gson
 
 @Composable
 fun BottomNavGraph(navController: NavHostController, modifier: Modifier = Modifier) {
     NavHost(navController, startDestination = BottomNavItem.Home.route, modifier) {
-        composable(BottomNavItem.Home.route) { HomeScreen() }
-        composable(BottomNavItem.Category.route) { CategoryScreen() }
-        composable(BottomNavItem.Diary.route) { DiaryScreen() }
-        composable(BottomNavItem.Bookmark.route) { BookmarkScreen() }
-        composable(BottomNavItem.My.route) { MyPageScreen() }
-        composable("details") { NewsDetailScreen() }
+        composable(BottomNavItem.Home.route) { HomeScreen(navController) }
+        composable(BottomNavItem.Category.route) { CategoryScreen(navController) }
+        composable(BottomNavItem.Diary.route) { DiaryScreen(navController) }
+        composable(BottomNavItem.Bookmark.route) { BookmarkScreen(navController) }
+        composable(BottomNavItem.My.route) { MyPageScreen(navController) }
+        composable(
+            "details/{newsId}",
+        ) { backStackEntry ->
+            val newsId = backStackEntry.arguments?.getString("newsId")
+            newsId?.let { NewsDetailScreen(it, navController) }
 
+        }
     }
 }
