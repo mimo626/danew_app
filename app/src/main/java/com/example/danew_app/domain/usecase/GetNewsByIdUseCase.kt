f47e325727030adb6b90eb9b80334117c480f17d
kangminju <yessuk5499@gmail.com>
2025-08-11 01:39:23 +0900

    GetNewsByIdUseCase 생성

---
+++ app/src/main/java/com/example/danew_app/domain/usecase/GetNewsByIdUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.danew_app.domain.usecase
+
+import com.example.danew_app.domain.model.NewsModel
+import com.example.danew_app.domain.repository.NewsRepository
+import javax.inject.Inject
+
+class GetNewsByIdUseCase @Inject constructor(
+    private val repository: NewsRepository
+) {
+    suspend operator fun invoke(id: String): List<NewsModel> {
+        return repository.getNewsById(id)
+    }
+}
