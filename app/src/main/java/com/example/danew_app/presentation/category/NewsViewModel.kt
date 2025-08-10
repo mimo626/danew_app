262026d757072ffb1ea66e17d5d9b5f84ae5823b
kangminju <yessuk5499@gmail.com>
2025-08-11 01:40:33 +0900

    NewsViewModel에 getNewsByIdUseCase 정의

--- app/src/main/java/com/example/danew_app/presentation/category/NewsViewModel.kt
+++ app/src/main/java/com/example/danew_app/presentation/category/NewsViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.danew_app.presentation.category
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
@@ -8,16 +9,23 @@ import kotlinx.coroutines.launch
 import androidx.compose.runtime.setValue
 import com.example.danew_app.domain.model.NewsModel
 import com.example.danew_app.domain.usecase.GetNewsByCategoryUseCase
+import com.example.danew_app.domain.usecase.GetNewsByIdUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class NewsViewModel @Inject constructor(
-    private val getNewsByCategoryUseCase: GetNewsByCategoryUseCase
+    private val getNewsByCategoryUseCase: GetNewsByCategoryUseCase,
+    private val getNewsByIdUseCase: GetNewsByIdUseCase
 ) : ViewModel() {
 
     var newsList by mutableStateOf<List<NewsModel>>(emptyList())
         private set
+
+    private val _newsListById = MutableStateFlow<List<NewsModel>>(emptyList())
+    val newsListById: StateFlow<List<NewsModel>> = _newsListById
 
     var isLoading by mutableStateOf(false)
         private set
@@ -38,5 +46,24 @@ class NewsViewModel @Inject constructor(
             }
         }
     }
+
+    fun fetchNewsById(id: String) {
+        viewModelScope.launch {
+            isLoading = true
+            errorMessage = null
+            try {
+                viewModelScope.launch {
+                    val data = getNewsByIdUseCase(id)
+                    _newsListById.value = data
+                    Log.d("NewsViewModel", "newsListById: $data")
+                }
+            } catch (e: Exception) {
+                errorMessage = e.localizedMessage
+            } finally {
+                isLoading = false
+            }
+        }
+    }
+
 }
 
