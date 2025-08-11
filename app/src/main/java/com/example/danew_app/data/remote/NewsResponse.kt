1fb3fef99913bf5bd36b12bc5c8e97390d3fe272
kangminju <yessuk5499@gmail.com>
2025-08-11 17:03:42 +0900

    NewsResponse 필드 추가

--- app/src/main/java/com/example/danew_app/data/remote/NewsResponse.kt
+++ app/src/main/java/com/example/danew_app/data/remote/NewsResponse.kt
@@ -5,6 +5,12 @@ import com.google.gson.annotations.SerializedName
 
 //API 응답을 받을 때 사용하는 DTO (Data Transfer Object).
 data class NewsResponse(
+    @SerializedName("totalResults")
+    val totalResults: Int,
+
+    @SerializedName("nextPage")
+    val nextPage: String?,
+
     @SerializedName("results")
-    val results: List<NewsEntity>
+    val results: List<NewsEntity>,
 )
