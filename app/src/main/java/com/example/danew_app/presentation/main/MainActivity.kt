d8a76a40a2ad5fdab9dd83206175962ebc4f67d9
kangminju <yessuk5499@gmail.com>
2025-08-11 19:37:52 +0900

    MainActivity 수정

--- app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
@@ -1,16 +1,15 @@
 package com.example.danew_app.presentation.main
 
+import MainScreen
+import RootNavGraph
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
-import com.example.danew.core.componet.BottomNavBar
-import com.example.danew.core.navigation.BottomNavGraph
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,17 +18,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DanewApp()
+            val rootNavController = rememberNavController()
+            Scaffold { innerPadding ->
+                RootNavGraph(
+                    navController = rootNavController,
+                    modifier = Modifier.padding(innerPadding)
+                )
+            }
         }
-    }
-}
-
-@Composable
-fun DanewApp() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { BottomNavBar(navController) }
-    ) { innerPadding ->
-        BottomNavGraph(navController = navController, modifier = Modifier.padding(innerPadding))
     }
 }
