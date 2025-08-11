8fa89c562299c695829bbd20b8bba1c0202cd536
kangminju <yessuk5499@gmail.com>
2025-08-12 01:44:26 +0900

    navController 수정

--- app/src/main/java/com/example/danew_app/presentation/home/NewsDetailScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/home/NewsDetailScreen.kt
@@ -14,9 +14,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
@@ -31,7 +29,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -43,7 +40,7 @@ import com.example.danew_app.presentation.category.NewsViewModel
 import com.example.danew_app.presentation.home.MainTopAppBar
 
 @Composable
-fun NewsDetailScreen(newsId: String, navController: NavHostController) {
+fun NewsDetailScreen(newsId: String, navHostController: NavHostController) {
     val viewModel: NewsViewModel = hiltViewModel()
     val newsList by viewModel.newsListById.collectAsState()
     val isLoading = viewModel.isLoading
@@ -55,7 +52,7 @@ fun NewsDetailScreen(newsId: String, navController: NavHostController) {
 
     Scaffold (
         topBar = {
-            MainTopAppBar(navController = navController, title = "", icon = null,
+            MainTopAppBar(navController = navHostController, title = "", icon = null,
                 isHome = false, isBackIcon = true)
         }
     ){
