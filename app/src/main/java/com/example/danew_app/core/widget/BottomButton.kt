5c49a7dd1597ad8479a867a0e012fbcd18c3dcaa
kangminju <yessuk5499@gmail.com>
2025-08-12 01:42:24 +0900

    BottomButton 생성

---
+++ app/src/main/java/com/example/danew_app/core/widget/BottomButton.kt
@@ -0,0 +1,37 @@
+package com.example.danew_app.core.widget
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.danew_app.core.theme.ColorsLight
+
+@Composable
+fun BottomButton(
+    text: String,
+    textColor: Color = Color.White,
+    backgroundColor: Color = ColorsLight.primaryColor,
+    onClick: () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 20.dp, vertical = 12.dp)
+            .height(48.dp),
+        colors = ButtonDefaults.buttonColors(containerColor = backgroundColor)
+    ) {
+        Text(
+            text = text,
+            color = textColor,
+            fontSize = 16.sp
+        )
+    }
+}
