08ab8a19786cde9366d93b740e1b76bc0a3c7b3d
kangminju <yessuk5499@gmail.com>
2025-08-13 01:22:38 +0900

    User 구현

--- app/src/main/java/com/example/danew_app/domain/model/User.kt
+++ app/src/main/java/com/example/danew_app/domain/model/User.kt
@@ -1,8 +1,14 @@
 package com.example.danew.domain.model
 
-class User {
-    var userId: String = ""
-    var password: String = ""
-    var name: String = "null"
-    var age: Int = 0
-}
+data class User (
+    val userId: String,
+    val password: String,
+    val name: String,
+    val age: Int,
+    val keyWordList: List<String>,
+    val diaryContent: String?,
+    // String 뉴스 아이디를 저장해서 모음
+    val bookmarkNewsList: List<String>?,
+    // 검색한 키워드, 클릭한 뉴스의 키워드를 모아서 맞춤형 뉴스 추천
+    val customNewsKeywordList: List<String>?,
+)
