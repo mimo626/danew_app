07b3daade94dd0725a28278b82302da85a91dbee
kangminju <yessuk5499@gmail.com>
2025-08-13 01:21:03 +0900

    SearchBar 생성

--- app/src/main/java/com/example/danew_app/presentation/home/SearchBar.kt
+++ app/src/main/java/com/example/danew_app/presentation/home/SearchBar.kt
@@ -1,7 +1,14 @@
 package com.example.danew_app.presentation.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -10,31 +17,34 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
 import com.example.danew_app.core.theme.ColorsLight
 
 //검색바
 @Composable
-fun SearchBar() {
-    TextField(
-        value = "",
-        onValueChange = {},
-        placeholder = { Text("검색어를 입력해 주세요.") },
+fun SearchBar(navHostController: NavHostController) {
+    Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-        leadingIcon = {
-            Icon(Icons.Default.Search, contentDescription = "검색")
-        },
-        shape = RoundedCornerShape(16.dp),
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = ColorsLight.lightGrayColor,    // 포커스된 상태의 배경색
-            unfocusedContainerColor = ColorsLight.lightGrayColor,  // 포커스되지 않은 상태의 배경색
-            disabledContainerColor = ColorsLight.lightGrayColor,
-            unfocusedIndicatorColor = Color.Transparent,
-            focusedIndicatorColor = Color.Transparent
-        )
-    )
+            .padding(horizontal = 20.dp)
+            .clickable {
+                navHostController.navigate("search")
+            }
+            .background(ColorsLight.lightGrayColor, RoundedCornerShape(16.dp))
+            .padding(horizontal = 16.dp, vertical = 12.dp)
+    ) {
+        Row( modifier = Modifier
+            .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,)
+        {
+            Text("검색어를 입력해 주세요.", color = ColorsLight.grayColor)
+            Icon(Icons.Default.Search, contentDescription = "검색", tint = Color.Gray)
+
+        }
+    }
+
 }
