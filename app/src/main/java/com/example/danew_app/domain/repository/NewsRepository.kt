44edbd1b12130f1059cd1f9560d18213e19f95e7
kangminju <yessuk5499@gmail.com>
2025-08-21 03:33:19 +0900

    NewsRepository 수정

--- app/src/main/java/com/example/danew_app/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/danew_app/domain/repository/NewsRepository.kt
@@ -1,12 +1,6 @@
 package com.example.danew_app.domain.repository
 
 import com.example.danew_app.data.entity.NewsEntity
-import com.example.danew_app.data.repository.NewsRepositoryImpl
-import com.example.danew_app.domain.model.NewsModel
-import dagger.Binds
-import dagger.Module
-import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 
 interface NewsRepository {
     suspend fun getNewsByCategory(category: String, loadMore:Boolean): List<NewsEntity>
@@ -14,12 +8,3 @@ interface NewsRepository {
     suspend fun getNewsBySearchQuery(searchQuery: String, loadMore:Boolean): List<NewsEntity>
 }
 
-@Module
-@InstallIn(SingletonComponent::class)
-abstract class RepositoryModule {
-
-    @Binds
-    abstract fun bindNewsRepository(
-        impl: NewsRepositoryImpl
-    ): NewsRepository
-}
