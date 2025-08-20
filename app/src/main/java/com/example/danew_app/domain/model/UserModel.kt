5233b112ba7399358b03b8d363ee644ac53450d5
kangminju <yessuk5499@gmail.com>
2025-08-21 03:31:56 +0900

    UserModel 수정

--- app/src/main/java/com/example/danew_app/domain/model/UserModel.kt
+++ app/src/main/java/com/example/danew_app/domain/model/UserModel.kt
@@ -1,15 +1,12 @@
-package com.example.danew.domain.model
+import kotlinx.datetime.*
 
-import kotlinx.datetime.LocalDateTime
-
-data class UserModel (
-    val userId: String,
-    val password: String,
-    val name: String,
-    val age: Int,
-    val gender: String,
-    val createdAt: LocalDateTime,
-    val keyWordList: List<String>,
-    // 검색한 키워드, 클릭한 뉴스의 키워드를 모아서 맞춤형 뉴스 추천
-    val customList: List<String>?,
+data class UserModel(
+    val userId: String = "",
+    val password: String = "",
+    val name: String = "",
+    val age: Int = 0,
+    val gender: String = "",
+    val createdAt: String = "",
+    val keywordList: List<String> = emptyList(),
+    val customList: List<String>? = emptyList()
 )
