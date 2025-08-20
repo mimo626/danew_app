152c5b406e9e425c117bf9fbca898ce0596a8cff
kangminju <yessuk5499@gmail.com>
2025-08-20 17:13:31 +0900

    유저 모델 수정

--- app/src/main/java/com/example/danew_app/domain/model/User.kt
+++ app/src/main/java/com/example/danew_app/domain/model/User.kt
@@ -1,14 +1,15 @@
 package com.example.danew.domain.model
 
+import kotlinx.datetime.LocalDateTime
+
 data class User (
     val userId: String,
     val password: String,
     val name: String,
     val age: Int,
+    val gender: String,
+    val createdAt: LocalDateTime,
     val keyWordList: List<String>,
-    val diaryContent: String?,
-    // String 뉴스 아이디를 저장해서 모음
-    val bookmarkNewsList: List<String>?,
     // 검색한 키워드, 클릭한 뉴스의 키워드를 모아서 맞춤형 뉴스 추천
-    val customNewsKeywordList: List<String>?,
+    val customList: List<String>?,
 )
