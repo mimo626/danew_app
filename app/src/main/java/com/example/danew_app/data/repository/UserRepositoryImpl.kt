5129c3957101ecfc3cbb787879b998006ad309e1
kangminju <yessuk5499@gmail.com>
2025-08-21 03:32:09 +0900

    UserRepositoryImpl 구현

---
+++ app/src/main/java/com/example/danew_app/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.example.danew_app.data.repository
+
+import UserModel
+import android.util.Log
+import com.example.danew.data.local.entity.UserEntity
+import com.example.danew_app.data.remote.UserApi
+import com.example.danew_app.domain.repository.UserRepository
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class UserRepositoryImpl @Inject constructor(
+    private val api: UserApi
+) : UserRepository {
+    override suspend fun insertUser(user: UserEntity) {
+        Log.d("User", "UserRepositoryImpl_insertUser: $user")
+        api.insert(user)
+    }
+}
