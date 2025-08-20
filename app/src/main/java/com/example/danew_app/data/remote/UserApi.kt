64290537ef478a2a8555b600c7bb87fc2d0ef454
kangminju <yessuk5499@gmail.com>
2025-08-21 03:31:17 +0900

    UserApi 생성

---
+++ app/src/main/java/com/example/danew_app/data/remote/UserApi.kt
@@ -0,0 +1,12 @@
+package com.example.danew_app.data.remote//package com.example.danew.data.repository.retrofit;
+
+import com.example.danew.data.local.entity.UserEntity
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+interface UserApi {
+    @POST("api/auth/signup")
+    fun insert(@Body user: UserEntity): Call<UserEntity>
+}
