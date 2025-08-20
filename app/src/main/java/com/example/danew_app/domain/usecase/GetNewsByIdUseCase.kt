eb76998694e2f69075a451ce2ff7358085478b9f
kangminju <yessuk5499@gmail.com>
2025-08-20 17:12:29 +0900

    모델로 변환

--- app/src/main/java/com/example/danew_app/domain/usecase/GetNewsByIdUseCase.kt
+++ app/src/main/java/com/example/danew_app/domain/usecase/GetNewsByIdUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.danew_app.domain.usecase
 
+import com.example.danew_app.data.mapper.toDomain
 import com.example.danew_app.domain.model.NewsModel
 import com.example.danew_app.domain.repository.NewsRepository
 import javax.inject.Inject
@@ -8,6 +9,6 @@ class GetNewsByIdUseCase @Inject constructor(
     private val repository: NewsRepository
 ) {
     suspend operator fun invoke(id: String): List<NewsModel> {
-        return repository.getNewsById(id)
+        return repository.getNewsById(id).map { it.toDomain() }
     }
 }
