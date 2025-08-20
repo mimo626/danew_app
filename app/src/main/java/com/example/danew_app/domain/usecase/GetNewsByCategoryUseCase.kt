afad2f9894b35773e964f641d3f1b2e0dc8448da
kangminju <yessuk5499@gmail.com>
2025-08-20 17:12:25 +0900

    도메인 변환

--- app/src/main/java/com/example/danew_app/domain/usecase/GetNewsByCategoryUseCase.kt
+++ app/src/main/java/com/example/danew_app/domain/usecase/GetNewsByCategoryUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.danew_app.domain.usecase
 
+import com.example.danew_app.data.mapper.toDomain
 import com.example.danew_app.domain.model.NewsModel
 import com.example.danew_app.domain.repository.NewsRepository
 import javax.inject.Inject
@@ -8,6 +9,6 @@ class GetNewsByCategoryUseCase @Inject constructor(
     private val repository: NewsRepository
 ) {
     suspend operator fun invoke(category: String, loadMore:Boolean=false): List<NewsModel> {
-        return repository.getNewsByCategory(category, loadMore)
+        return repository.getNewsByCategory(category, loadMore).map { it.toDomain() }
     }
 }
