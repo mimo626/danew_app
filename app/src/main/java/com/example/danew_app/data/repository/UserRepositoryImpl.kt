9c1d00fea0df062f82a9ba9287eab74443561cab
kangminju <yessuk5499@gmail.com>
2025-08-21 13:36:18 +0900

    api 결과 확인

--- app/src/main/java/com/example/danew_app/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/danew_app/data/repository/UserRepositoryImpl.kt
@@ -4,6 +4,9 @@ import android.util.Log
 import com.example.danew.data.local.entity.UserEntity
 import com.example.danew_app.data.remote.UserApi
 import com.example.danew_app.domain.repository.UserRepository
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -13,6 +16,14 @@ class UserRepositoryImpl @Inject constructor(
 ) : UserRepository {
     override suspend fun insertUser(user: UserEntity) {
         Log.d("User", "UserRepositoryImpl_insertUser: $user")
-        api.insert(user)
+        api.save(user).enqueue(object : Callback<UserEntity> {
+            override fun onResponse(call: Call<UserEntity>, response: Response<UserEntity>) {
+                Log.i("signup", "성공: ${response.body()}")
+            }
+
+            override fun onFailure(call: Call<UserEntity>, t: Throwable) {
+                Log.e("signup", "실패", t)
+            }
+        })
     }
 }
