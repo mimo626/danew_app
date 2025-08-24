6a938d11d1701a0c7334a22178625d17cdea0927
kangminju <yessuk5499@gmail.com>
2025-08-24 22:05:34 +0900

    로그인 함수 수정

--- app/src/main/java/com/example/danew_app/domain/repository/UserRepository.kt
+++ app/src/main/java/com/example/danew_app/domain/repository/UserRepository.kt
@@ -1,10 +1,11 @@
 package com.example.danew_app.domain.repository
 
 import com.example.danew.data.local.entity.UserEntity
+import com.example.danew_app.data.dto.LoginResponse
 
 
 interface UserRepository {
     suspend fun insertUser(user:UserEntity)
-    suspend fun login(userId: String, password: String)
+    suspend fun login(userId: String, password: String) : LoginResponse
     suspend fun checkUserId(userId: String, callback: (Boolean) -> Unit)
 }
