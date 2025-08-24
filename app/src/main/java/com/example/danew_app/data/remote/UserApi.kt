c6292cb2e79a98f933969115951a9ab9f69f595f
kangminju <yessuk5499@gmail.com>
2025-08-24 21:27:07 +0900

    로그인, 아이디 중복 체크 api 연결

--- app/src/main/java/com/example/danew_app/data/remote/UserApi.kt
+++ app/src/main/java/com/example/danew_app/data/remote/UserApi.kt
@@ -1,12 +1,22 @@
 package com.example.danew_app.data.remote//package com.example.danew.data.repository.retrofit;
 
 import com.example.danew.data.local.entity.UserEntity
+import com.example.danew_app.data.dto.LoginRequest
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET
 import retrofit2.http.POST;
+import retrofit2.http.Query
 
 interface UserApi {
     @POST("api/auth/signup")
     fun save(@Body user: UserEntity): Call<UserEntity>
+
+    @POST("api/auth/login")
+    fun login(@Body loginRequest: LoginRequest): Call<String>
+
+    @GET("api/auth/check-username")
+    fun checkUserId(@Query("userId") userId: String): Call<Boolean>
+
 }
