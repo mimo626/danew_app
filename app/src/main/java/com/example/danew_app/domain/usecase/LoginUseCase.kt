8f8e25767d0be8a5a310ab3849cfc01461a676bd
kangminju <yessuk5499@gmail.com>
2025-08-24 22:04:39 +0900

    LoginUseCase 수정

--- app/src/main/java/com/example/danew_app/domain/usecase/LoginUseCase.kt
+++ app/src/main/java/com/example/danew_app/domain/usecase/LoginUseCase.kt
@@ -1,14 +1,15 @@
 package com.example.danew_app.domain.usecase
 
 import android.util.Log
+import com.example.danew_app.data.dto.LoginResponse
 import com.example.danew_app.domain.repository.UserRepository
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
     private val repository: UserRepository
 ){
-    suspend operator fun invoke(userId: String, password:String){
+    suspend operator fun invoke(userId: String, password:String) : LoginResponse{
         Log.d("User", "LoginUseCase_invoke: $userId")
-        repository.login(userId, password)
+        return repository.login(userId, password)
     }
 }
