d952f1ccab689d0c2383c3942dde05994236889c
kangminju <yessuk5499@gmail.com>
2025-08-24 21:25:57 +0900

    LoginRequest 생성

---
+++ app/src/main/java/com/example/danew_app/data/dto/LoginRequest.kt
@@ -0,0 +1,11 @@
+package com.example.danew_app.data.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginRequest (
+    @SerializedName("userId")
+    val userId: String,
+
+    @SerializedName("password")
+    val password: String,
+)
