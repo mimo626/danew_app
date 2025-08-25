64ab10a342c5dde6b6ebfdef971830189731c119
kangminju <yessuk5499@gmail.com>
2025-08-26 00:08:48 +0900

    로그인 시 토큰, 로그인 여부 확인 구현

--- app/src/main/java/com/example/danew_app/domain/usecase/LoginUseCase.kt
+++ app/src/main/java/com/example/danew_app/domain/usecase/LoginUseCase.kt
@@ -1,13 +1,25 @@
 package com.example.danew_app.domain.usecase
 
 import com.example.danew_app.data.dto.LoginResponse
+import com.example.danew_app.data.local.UserDataSource
 import com.example.danew_app.domain.repository.UserRepository
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
-    private val repository: UserRepository
-){
-    suspend operator fun invoke(userId: String, password:String) : LoginResponse{
-        return repository.login(userId, password)
+    private val repository: UserRepository,
+    private val dataSource: UserDataSource,   // ✅ 추가
+) {
+    suspend operator fun invoke(userId: String, password: String): LoginResponse {
+        val response = repository.login(userId, password)
+
+        if (response.success && !response.token.isNullOrEmpty()) {
+            dataSource.saveLoginInfo(response.token)
+        }
+
+        return response
+    }
+
+    suspend fun isUserLoggedIn(): Boolean {
+        return dataSource.checkLoginState()
     }
 }
