c0e26e894ada6373f659a2f6984727a4217d825f
kangminju <yessuk5499@gmail.com>
2025-08-26 01:39:41 +0900

    SplashScreen 구현

---
+++ app/src/main/java/com/example/danew_app/presentation/main/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.example.danew_app.presentation.main
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.danew_app.core.theme.ColorsLight
+import com.example.danew_app.core.widget.BottomButton
+
+@Composable
+fun SplashScreen() {
+
+    Scaffold(
+        containerColor = ColorsLight.whiteColor,
+    ) {
+            padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding),
+            verticalArrangement = Arrangement.Center
+        )
+        {
+            Text("DANEW", style = TextStyle(fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                color = ColorsLight.primaryColor,),
+                modifier = Modifier.align(Alignment.CenterHorizontally) // Column 안에서는 가로 정렬
+            )
+        }
+    }
+}
