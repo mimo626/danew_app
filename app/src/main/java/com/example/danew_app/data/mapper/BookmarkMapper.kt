64465116169c707d473ed66013161d3a01ed9f09
kangminju <yessuk5499@gmail.com>
2025-08-27 00:12:25 +0900

    북마크 매퍼 생성

---
+++ app/src/main/java/com/example/danew_app/data/mapper/BookmarkMapper.kt
@@ -0,0 +1,11 @@
+package com.example.danew_app.data.mapper
+
+import com.example.danew_app.data.entity.BookmarkEntity
+import com.example.danew_app.domain.model.BookmarkModel
+
+fun BookmarkEntity.toDomain(): BookmarkModel {
+    return BookmarkModel(
+        userId = this.userId,
+        articleId = this.articleId,
+    )
+}
