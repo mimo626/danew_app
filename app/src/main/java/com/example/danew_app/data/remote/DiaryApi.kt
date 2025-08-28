c8be748d327e5afcd98e76adea767435ebdfa6c3
kangminju <yessuk5499@gmail.com>
2025-08-28 14:09:17 +0900

    다이어리 api 생성

---
+++ app/src/main/java/com/example/danew_app/data/remote/DiaryApi.kt
@@ -0,0 +1,20 @@
+package com.example.danew_app.data.remote
+
+import com.example.danew_app.data.dto.DiaryRequest
+import com.example.danew_app.data.entity.DiaryEntity
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface DiaryApi {
+    @POST("api/diary/save")
+    fun save(@Body diaryRequest: DiaryRequest,
+             @Header("Authorization") token:String): Call<DiaryEntity>
+
+    @GET("api/diary/getByDate")
+    fun getByDate(@Header("Authorization") token:String,
+                  @Query("createdAt") createdAt:String): Call<DiaryEntity>
+}
