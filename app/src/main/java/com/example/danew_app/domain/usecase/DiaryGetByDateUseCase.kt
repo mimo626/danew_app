bb687692ae963543740c4d90e4af8eddf99d5617
kangminju <yessuk5499@gmail.com>
2025-08-28 14:09:23 +0900

    DiaryGetByDateUseCase 구현

---
+++ app/src/main/java/com/example/danew_app/domain/usecase/DiaryGetByDateUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.danew_app.domain.usecase
+
+import com.example.danew_app.data.entity.DiaryEntity
+import com.example.danew_app.data.mapper.toDomain
+import com.example.danew_app.domain.model.DiaryModel
+import com.example.danew_app.domain.repository.DiaryRepository
+import javax.inject.Inject
+
+class DiaryGetByDateUseCase @Inject constructor(
+    private val diaryRepository: DiaryRepository,
+){
+    suspend operator fun invoke(token:String, createdAt:String) : DiaryModel {
+        return diaryRepository.getByDate(token, createdAt).toDomain()
+    }
+}
