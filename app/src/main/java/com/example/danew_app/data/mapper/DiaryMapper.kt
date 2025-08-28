d9f0b8aab0653d46e9b37753c2958ccf605a9826
kangminju <yessuk5499@gmail.com>
2025-08-28 16:34:56 +0900

    toDomain nullable하게 수정

--- app/src/main/java/com/example/danew_app/data/mapper/DiaryMapper.kt
+++ app/src/main/java/com/example/danew_app/data/mapper/DiaryMapper.kt
@@ -3,7 +3,8 @@ package com.example.danew_app.data.mapper
 import com.example.danew_app.data.entity.DiaryEntity
 import com.example.danew_app.domain.model.DiaryModel
 
-fun DiaryEntity.toDomain(): DiaryModel {
+fun DiaryEntity.toDomain(): DiaryModel? {
+    if (diaryId == null && userId == null && createdAt == null && content == null) return null
     return DiaryModel(
         diaryId = this.diaryId,
         createdAt = this.createdAt,
