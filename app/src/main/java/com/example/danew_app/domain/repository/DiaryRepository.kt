25f0a6ef9c271e84c2d9d0f0218ead9036ffd03b
kangminju <yessuk5499@gmail.com>
2025-08-28 16:35:01 +0900

    DiaryRepository nullable하게 수정

--- app/src/main/java/com/example/danew_app/domain/repository/DiaryRepository.kt
+++ app/src/main/java/com/example/danew_app/domain/repository/DiaryRepository.kt
@@ -3,6 +3,6 @@ package com.example.danew_app.domain.repository
 import com.example.danew_app.data.entity.DiaryEntity
 
 interface DiaryRepository {
-    suspend fun saveDiary(content:String, token:String):DiaryEntity
-    suspend fun getByDate(token: String, createdAt:String):DiaryEntity
+    suspend fun saveDiary(content:String, createdAt: String, token:String):DiaryEntity
+    suspend fun getByDate(token: String, createdAt:String):DiaryEntity?
 }
