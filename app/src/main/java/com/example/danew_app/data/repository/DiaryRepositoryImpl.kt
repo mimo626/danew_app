7b9dd2faaf695d5ca13d2cbaa2ed06d29b244ee9
kangminju <yessuk5499@gmail.com>
2025-08-28 14:09:38 +0900

    DiaryRepositoryImpl 구현

---
+++ app/src/main/java/com/example/danew_app/data/repository/DiaryRepositoryImpl.kt
@@ -0,0 +1,68 @@
+package com.example.danew_app.data.repository
+
+import android.util.Log
+import com.example.danew_app.data.dto.DiaryRequest
+import com.example.danew_app.data.entity.DiaryEntity
+import com.example.danew_app.data.remote.DiaryApi
+import com.example.danew_app.domain.repository.DiaryRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.suspendCancellableCoroutine
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DiaryRepositoryImpl @Inject constructor(
+    private val api: DiaryApi,
+) : DiaryRepository{
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override suspend fun saveDiary(content: String, token: String): DiaryEntity =
+        suspendCancellableCoroutine{ cont ->
+            api.save(DiaryRequest(content), token).enqueue(object : Callback<DiaryEntity>{
+                override fun onResponse(call: Call<DiaryEntity>, response: Response<DiaryEntity>) {
+                    if (response.isSuccessful){
+                        val body = response.body()
+                        if(body != null){
+                            cont.resume(body){}
+                        }
+                        else {
+                            Log.e("Diary 저장", "응답 바디 없음: ${response.code()}")
+                        }
+                    } else {
+                        Log.e("Diary 저장", "서버 오류: ${response.code()}")
+                    }
+                }
+
+                override fun onFailure(call: Call<DiaryEntity>, t: Throwable) {
+                    Log.e("Diary 저장", "네트워크 실패", t)
+                }
+            })
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override suspend fun getByDate(token: String, createdAt: String): DiaryEntity =
+        suspendCancellableCoroutine{ cont ->
+            api.getByDate(token, createdAt).enqueue(object : Callback<DiaryEntity>{
+                override fun onResponse(call: Call<DiaryEntity>, response: Response<DiaryEntity>) {
+                    if (response.isSuccessful){
+                        val body = response.body()
+                        if (body != null){
+                            cont.resume(body) {}
+                        } else{
+                            Log.e("Diary 날짜별 조회", "응답 바디 없음: ${response.code()}")
+                        }
+                    }else {
+                        Log.e("Diary 날짜별 조회", "서버 오류: ${response.code()}")
+                    }
+                }
+                override fun onFailure(call: Call<DiaryEntity>, t: Throwable) {
+                    Log.e("Diary 날짜별 조회", "네트워크 실패", t)
+                }
+            }
+            )
+
+    }
+}
