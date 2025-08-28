452cb9574e10b887f0316e094d5ef304c4c20d41
kangminju <yessuk5499@gmail.com>
2025-08-28 16:34:47 +0900

    DiaryGetByDateUseCase nullable하게 수정

--- app/src/main/java/com/example/danew_app/domain/usecase/DiaryGetByDateUseCase.kt
+++ app/src/main/java/com/example/danew_app/domain/usecase/DiaryGetByDateUseCase.kt
@@ -1,5 +1,6 @@
 package com.example.danew_app.domain.usecase
 
+import android.util.Log
 import com.example.danew_app.data.entity.DiaryEntity
 import com.example.danew_app.data.mapper.toDomain
 import com.example.danew_app.domain.model.DiaryModel
@@ -9,7 +10,7 @@ import javax.inject.Inject
 class DiaryGetByDateUseCase @Inject constructor(
     private val diaryRepository: DiaryRepository,
 ){
-    suspend operator fun invoke(token:String, createdAt:String) : DiaryModel {
-        return diaryRepository.getByDate(token, createdAt).toDomain()
+    suspend operator fun invoke(token:String, createdAt:String) : DiaryModel? {
+        return diaryRepository.getByDate(token, createdAt)?.toDomain()
     }
 }
