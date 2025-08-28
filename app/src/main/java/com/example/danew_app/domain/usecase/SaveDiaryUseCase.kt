193183f056a84c39efc212b774911bd403d77425
kangminju <yessuk5499@gmail.com>
2025-08-29 01:35:48 +0900

    SaveDiaryUseCase 반환값 수정

--- app/src/main/java/com/example/danew_app/domain/usecase/SaveDiaryUseCase.kt
+++ app/src/main/java/com/example/danew_app/domain/usecase/SaveDiaryUseCase.kt
@@ -1,14 +1,14 @@
 package com.example.danew_app.domain.usecase
 
-import android.util.Log
-import com.example.danew_app.data.entity.DiaryEntity
+import com.example.danew_app.data.mapper.toDomain
+import com.example.danew_app.domain.model.DiaryModel
 import com.example.danew_app.domain.repository.DiaryRepository
 import javax.inject.Inject
 
 class SaveDiaryUseCase @Inject constructor(
     private val diaryRepository: DiaryRepository
 ) {
-    suspend operator fun invoke(content:String, createdAt: String, token:String):DiaryEntity{
-        return diaryRepository.saveDiary(content, createdAt, token)
+    suspend operator fun invoke(content:String, createdAt: String, token:String): DiaryModel? {
+        return diaryRepository.saveDiary(content, createdAt, token).toDomain()
     }
 }
