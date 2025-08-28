3464861de0ae9904a3f8c70db93dc8512fb9ba40
kangminju <yessuk5499@gmail.com>
2025-08-28 17:50:40 +0900

    DiaryModel에 Parcelize 적용

--- app/src/main/java/com/example/danew_app/domain/model/DiaryModel.kt
+++ app/src/main/java/com/example/danew_app/domain/model/DiaryModel.kt
@@ -1,7 +1,10 @@
 package com.example.danew_app.domain.model
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class DiaryModel (
     val diaryId: String = "",
     val createdAt: String = "",
     val content: String? = "",
-)
+):Parcelable
