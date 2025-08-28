5d7d0dd68a4b14655f49ff9db5ec07ff419037f4
kangminju <yessuk5499@gmail.com>
2025-08-28 14:10:51 +0900

    DiaryViewModel 구현

---
+++ app/src/main/java/com/example/danew_app/presentation/viewmodel/DiaryViewModel.kt
@@ -0,0 +1,54 @@
+package com.example.danew_app.presentation.viewmodel
+
+import android.util.Log
+import android.view.View
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.danew_app.data.local.UserDataSource
+import com.example.danew_app.domain.model.DiaryModel
+import com.example.danew_app.domain.usecase.DiaryGetByDateUseCase
+import com.example.danew_app.domain.usecase.SaveDiaryUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DiaryViewModel @Inject constructor(
+    private val saveDiaryUseCase:SaveDiaryUseCase,
+    private val diaryGetByDateUseCase: DiaryGetByDateUseCase,
+    private val userDataSource: UserDataSource,
+) : ViewModel(){
+
+    var content by mutableStateOf<String>("")
+    var createdAt by mutableStateOf<String?>(null)
+
+    var saveSuccess by mutableStateOf(false)
+        private set
+
+    var isLoading by mutableStateOf(false)
+        private set
+
+    var errorMessage by mutableStateOf<String?>(null)
+        private set
+
+    fun saveDiary(){
+        viewModelScope.launch {
+            isLoading = true
+            errorMessage = null
+            try {
+                val token = userDataSource.getToken() ?: ""
+                val diary = saveDiaryUseCase.invoke(content, token)
+                saveSuccess = true
+                Log.i("Diary 저장", "${saveSuccess}: ${diary}")
+            } catch (e:Exception){
+                errorMessage = e.localizedMessage
+            } finally {
+                isLoading = false
+            }
+        }
+    }
+}
