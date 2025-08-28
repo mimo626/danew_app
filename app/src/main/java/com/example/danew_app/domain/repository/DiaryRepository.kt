4ae38d38d185c25fd736b9f63011ed8fb44072cc
kangminju <yessuk5499@gmail.com>
2025-08-29 01:34:39 +0900

    수정 함수 구현

--- app/src/main/java/com/example/danew_app/domain/repository/DiaryRepository.kt
+++ app/src/main/java/com/example/danew_app/domain/repository/DiaryRepository.kt
@@ -1,8 +1,10 @@
 package com.example.danew_app.domain.repository
 
+import com.example.danew_app.data.dto.DiaryRequest
 import com.example.danew_app.data.entity.DiaryEntity
 
 interface DiaryRepository {
     suspend fun saveDiary(content:String, createdAt: String, token:String):DiaryEntity
     suspend fun getByDate(token: String, createdAt:String):DiaryEntity?
+    suspend fun updateDiary(token:String, diaryId:String, diaryRequest: DiaryRequest): DiaryEntity
 }
