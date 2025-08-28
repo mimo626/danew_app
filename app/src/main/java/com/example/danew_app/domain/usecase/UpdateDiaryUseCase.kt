a36c084175095b7a9eebc4eb9c4db93a06827c5d
kangminju <yessuk5499@gmail.com>
2025-08-29 01:35:37 +0900

    UpdateDiaryUseCase 구현

---
+++ app/src/main/java/com/example/danew_app/domain/usecase/UpdateDiaryUseCase.kt
@@ -0,0 +1,16 @@
+package com.example.danew_app.domain.usecase
+
+import com.example.danew_app.data.dto.DiaryRequest
+import com.example.danew_app.data.entity.DiaryEntity
+import com.example.danew_app.data.mapper.toDomain
+import com.example.danew_app.domain.model.DiaryModel
+import com.example.danew_app.domain.repository.DiaryRepository
+import javax.inject.Inject
+
+class UpdateDiaryUseCase @Inject constructor(
+    private val diaryRepository: DiaryRepository,
+){
+    suspend operator fun invoke(diaryId:String, diaryRequest: DiaryRequest, token:String): DiaryModel? {
+        return diaryRepository.updateDiary(token, diaryId, diaryRequest).toDomain()
+    }
+}
