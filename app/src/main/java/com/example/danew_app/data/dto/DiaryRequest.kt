d7e223d0e7e200e8375055348aff409cc3402210
kangminju <yessuk5499@gmail.com>
2025-08-28 16:35:27 +0900

    DiaryRequest createdAt 추가

--- app/src/main/java/com/example/danew_app/data/dto/DiaryRequest.kt
+++ app/src/main/java/com/example/danew_app/data/dto/DiaryRequest.kt
@@ -5,4 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class DiaryRequest (
     @SerializedName("content")
     val content : String,
+    @SerializedName("createdAt")
+    val createdAt : String,
 )
