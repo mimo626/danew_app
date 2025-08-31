53cc598eb0104c2300df900050be05d6b2ab6733
kangminju <yessuk5499@gmail.com>
2025-09-01 03:00:16 +0900

    NewsMapper 추가

--- app/src/main/java/com/example/danew_app/data/mapper/NewsMapper.kt
+++ app/src/main/java/com/example/danew_app/data/mapper/NewsMapper.kt
@@ -1,5 +1,6 @@
 package com.example.danew_app.data.mapper
 
+import com.example.danew_app.data.entity.MetaNewsEntity
 import com.example.danew_app.data.entity.NewsEntity
 import com.example.danew_app.domain.model.NewsModel
 
@@ -14,5 +15,57 @@ fun NewsEntity.toDomain(): NewsModel {
         category = this.category,
         link = this.link,
         creator = this.creator,
+        language = this.language,
+        keywords = this.keywords,
+    )
+}
+
+fun MetaNewsEntity.toDomain(): NewsModel {
+    return NewsModel(
+        id = this.id,
+        title = this.title,
+        description = this.description,
+        imageUrl = this.imageUrl,
+        sourceName = this.sourceName,
+        pubDate = this.pubDate,
+        category = this.category,
+        link = this.link,
+        creator = this.creator,
+        language = this.language,
+        keywords = this.keywords,
+    )
+}
+
+fun NewsEntity.toMeta():MetaNewsEntity{
+    return MetaNewsEntity(
+        id = this.article_id ?: "",
+        title = this.title ?: "제목 없음",
+        description = this.description ?: "설명이 없습니다.",
+        imageUrl = this.image_url,
+        sourceName = this.source_name ?: "알 수 없음",
+        pubDate = this.pubDate,
+        category = this.category,
+        link = this.link,
+        creator = this.creator,
+        language = this.language,
+        keywords = this.keywords,
+        savedAt = ""
+    )
+}
+
+fun NewsModel.toMeta():MetaNewsEntity{
+    return MetaNewsEntity(
+        id = this.id,
+        title = this.title,
+        description = this.description,
+        imageUrl = this.imageUrl,
+        sourceName = this.sourceName,
+        pubDate = this.pubDate,
+        category = this.category,
+        link = this.link,
+        creator = this.creator,
+        language = this.language,
+        keywords = this.keywords,
+        savedAt = ""
     )
 }
