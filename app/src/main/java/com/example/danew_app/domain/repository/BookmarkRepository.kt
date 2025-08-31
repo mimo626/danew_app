68f33c8632dff23b643586f88444bd17887b27e3
kangminju <yessuk5499@gmail.com>
2025-09-01 02:59:24 +0900

    BookmarkRepository 구현

---
+++ app/src/main/java/com/example/danew_app/domain/repository/BookmarkRepository.kt
@@ -0,0 +1,9 @@
+package com.example.danew_app.domain.repository
+
+import com.example.danew_app.data.entity.BookmarkEntity
+import com.example.danew_app.data.entity.MetaNewsEntity
+
+interface BookmarkRepository {
+    suspend fun saveBookmark(token:String, metaNewsEntity: MetaNewsEntity):BookmarkEntity
+    suspend fun getBookmarks(token: String):List<MetaNewsEntity>
+}
