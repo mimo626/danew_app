873ac6bd92553f6aad09344af8d718b4c9999797
kangminju <yessuk5499@gmail.com>
2025-09-01 03:46:13 +0900

    deleteBookmark 함수 정의

--- app/src/main/java/com/example/danew_app/domain/repository/BookmarkRepository.kt
+++ app/src/main/java/com/example/danew_app/domain/repository/BookmarkRepository.kt
@@ -6,4 +6,5 @@ import com.example.danew_app.data.entity.MetaNewsEntity
 interface BookmarkRepository {
     suspend fun saveBookmark(token:String, metaNewsEntity: MetaNewsEntity):BookmarkEntity
     suspend fun getBookmarks(token: String):List<MetaNewsEntity>
+    suspend fun deleteBookmark(token:String, newsId:String):Boolean
 }
