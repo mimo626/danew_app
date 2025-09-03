7c2e69e258b2b834ba7f3adfa81a04bb676a22bc
kangminju <yessuk5499@gmail.com>
2025-09-03 15:42:34 +0900

    LoginUseCase 수정

--- app/src/main/java/com/example/danew_app/domain/usecase/LoginUseCase.kt
+++ app/src/main/java/com/example/danew_app/domain/usecase/LoginUseCase.kt
@@ -1,6 +1,6 @@
 package com.example.danew_app.domain.usecase
 
-import com.example.danew_app.data.dto.LoginResponse
+import com.example.danew_app.data.dto.UserResponse
 import com.example.danew_app.data.local.UserDataSource
 import com.example.danew_app.domain.repository.UserRepository
 import javax.inject.Inject
@@ -9,10 +9,10 @@ class LoginUseCase @Inject constructor(
     private val repository: UserRepository,
     private val dataSource: UserDataSource,   // ✅ 추가
 ) {
-    suspend operator fun invoke(userId: String, password: String): LoginResponse {
+    suspend operator fun invoke(userId: String, password: String): UserResponse {
         val response = repository.login(userId, password)
 
-        if (response.success && !response.token.isNullOrEmpty()) {
+        if (response.token.isNotEmpty()) {
             dataSource.saveLoginInfo(response.token)
         }
 
