467ecaa7fdcc7cf98d1784518528b5be60034196
kangminju <yessuk5499@gmail.com>
2025-09-03 18:55:37 +0900

    다이어리 api 함수 수정

--- app/src/main/java/com/example/danew_app/data/repository/DiaryRepositoryImpl.kt
+++ app/src/main/java/com/example/danew_app/data/repository/DiaryRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.danew_app.data.repository
 
 import android.util.Log
+import com.example.danew_app.data.dto.ApiResponse
 import com.example.danew_app.data.dto.DiaryRequest
 import com.example.danew_app.data.entity.DiaryEntity
 import com.example.danew_app.data.remote.DiaryApi
@@ -12,6 +13,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.coroutines.resumeWithException
 
 @Singleton
 class DiaryRepositoryImpl @Inject constructor(
@@ -21,24 +23,29 @@ class DiaryRepositoryImpl @Inject constructor(
     @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun saveDiary(content: String, createdAt: String, token: String): DiaryEntity =
         suspendCancellableCoroutine{ cont ->
-            api.save(DiaryRequest(content, createdAt), token).enqueue(object : Callback<DiaryEntity>{
-                override fun onResponse(call: Call<DiaryEntity>, response: Response<DiaryEntity>) {
-                    if (response.isSuccessful){
-                        val body = response.body()
-                        if(body != null){
-                            cont.resume(body){}
-                            Log.i("Diary 저장", "${response.body()}")
+            api.save(DiaryRequest(content, createdAt), token).enqueue(object : Callback<ApiResponse<DiaryEntity>>{
+                override fun onResponse(call: Call<ApiResponse<DiaryEntity>>, response: Response<ApiResponse<DiaryEntity>>) {
+                    val body = response.body()
+                    if (response.isSuccessful && body != null){
+                        if (body.status == "success" && body.data != null) {
+                            val diary = body.data
+                            cont.resume(diary){}
+                            Log.i("Diary 저장", "${diary}")
 
                         }
                         else {
-                            Log.e("Diary 저장", "응답 바디 없음: ${response.code()}")
+                            val msg = body.message ?: "다이어리 저장 실패"
+                            cont.resumeWithException(RuntimeException(msg))
+                            Log.e("Diary 저장", "실패: $msg")
                         }
                     } else {
-                        Log.e("Diary 저장", "서버 오류: ${response.code()}")
-                    }
+                        val errorMsg = response.errorBody()?.string() ?: "다이어리 저장 실패"
+                        cont.resumeWithException(RuntimeException(errorMsg))
+                        Log.e("Diary 저장", "실패: $errorMsg")                    }
                 }
 
-                override fun onFailure(call: Call<DiaryEntity>, t: Throwable) {
+                override fun onFailure(call: Call<ApiResponse<DiaryEntity>>, t: Throwable) {
+                    cont.resumeWithException(t)
                     Log.e("Diary 저장", "네트워크 실패", t)
                 }
             })
@@ -47,47 +54,60 @@ class DiaryRepositoryImpl @Inject constructor(
     @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun getByDate(token: String, createdAt: String): DiaryEntity? =
         suspendCancellableCoroutine{ cont ->
-            api.getByDate(token, createdAt).enqueue(object : Callback<DiaryEntity?>{
-                override fun onResponse(call: Call<DiaryEntity?>, response: Response<DiaryEntity?>) {
-                    if (response.isSuccessful){
-                        val body = response.body()
-                        if (body != null){
-                            cont.resume(body) {}
-                            Log.i("Diary 날짜별 조회", "성공: ${body}")
-                        } else{
-                            cont.resume(null) {} // 실패 시 null 처리
-                            Log.e("Diary 날짜별 조회", "응답 바디 없음: ${response.code()}")
+            api.getByDate(token, createdAt).enqueue(object : Callback<ApiResponse<DiaryEntity?>>{
+                override fun onResponse(call: Call<ApiResponse<DiaryEntity?>>, response: Response<ApiResponse<DiaryEntity?>>) {
+                    val body = response.body()
+                    if (response.isSuccessful && body != null){
+                        if (body.status == "success" && body.data != null) {
+                            val diary = body.data
+                            cont.resume(diary){}
+                            Log.i("Diary 날짜별 조회", "${diary}")
+
                         }
-                    }else {
-                        Log.e("Diary 날짜별 조회", "서버 오류: ${response.code()}")
-                    }
+                        else {
+                            val msg = body.message ?: "다이어리 날짜별 조회 실패"
+                            cont.resumeWithException(RuntimeException(msg))
+                            Log.e("Diary 날짜별 조회", "실패: $msg")
+                        }
+                    } else {
+                        val errorMsg = response.errorBody()?.string() ?: "다이어리 날짜별 조회 실패"
+                        cont.resumeWithException(RuntimeException(errorMsg))
+                        Log.e("Diary 날짜별 조회", "실패: $errorMsg")                    }
                 }
-                override fun onFailure(call: Call<DiaryEntity?>, t: Throwable) {
-                    cont.resume(null) {} // 실패 시 null 처리
+
+                override fun onFailure(call: Call<ApiResponse<DiaryEntity?>>, t: Throwable) {
+                    cont.resumeWithException(t)
                     Log.e("Diary 날짜별 조회", "네트워크 실패", t)
                 }
-            }
-            )
+            })
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun updateDiary(token:String, diaryId:String, diaryRequest: DiaryRequest): DiaryEntity =
         suspendCancellableCoroutine { cont ->
-            api.updateDiary(token, diaryId, diaryRequest).enqueue(object : Callback<DiaryEntity>{
-                override fun onResponse(call: Call<DiaryEntity>, response: Response<DiaryEntity>) {
-                    if (response.isSuccessful){
-                        val body = response.body()
-                        if (body != null){
-                            cont.resume(body) {}
-                            Log.i("Diary 수정", "성공: ${body}")
-                        } else{
-                            Log.e("Diary 수정", "응답 바디 없음: ${response.code()}")
+            api.updateDiary(token, diaryId, diaryRequest).enqueue(object : Callback<ApiResponse<DiaryEntity>>{
+                override fun onResponse(call: Call<ApiResponse<DiaryEntity>>, response: Response<ApiResponse<DiaryEntity>>) {
+                    val body = response.body()
+                    if (response.isSuccessful && body != null){
+                        if (body.status == "success" && body.data != null) {
+                            val diary = body.data
+                            cont.resume(diary){}
+                            Log.i("Diary 수정", "${diary}")
+
                         }
-                    }else {
-                        Log.e("Diary 수정", "서버 오류: ${response.code()}")
-                    }
+                        else {
+                            val msg = body.message ?: "다이어리 수정 실패"
+                            cont.resumeWithException(RuntimeException(msg))
+                            Log.e("Diary 수정", "실패: $msg")
+                        }
+                    } else {
+                        val errorMsg = response.errorBody()?.string() ?: "다이어리 수정 실패"
+                        cont.resumeWithException(RuntimeException(errorMsg))
+                        Log.e("Diary 수정", "실패: $errorMsg")                    }
                 }
-                override fun onFailure(call: Call<DiaryEntity>, t: Throwable) {
+
+                override fun onFailure(call: Call<ApiResponse<DiaryEntity>>, t: Throwable) {
+                    cont.resumeWithException(t)
                     Log.e("Diary 수정", "네트워크 실패", t)
                 }
             })
