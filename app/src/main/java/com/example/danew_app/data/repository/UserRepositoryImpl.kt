81822966f08873419579ff188241c8a6f3a407f7
kangminju <yessuk5499@gmail.com>
2025-09-03 15:41:44 +0900

    함수 전체 수정

--- app/src/main/java/com/example/danew_app/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/danew_app/data/repository/UserRepositoryImpl.kt
@@ -2,8 +2,9 @@ package com.example.danew_app.data.repository
 
 import android.util.Log
 import com.example.danew.data.local.entity.UserEntity
+import com.example.danew_app.data.dto.ApiResponse
 import com.example.danew_app.data.dto.LoginRequest
-import com.example.danew_app.data.dto.LoginResponse
+import com.example.danew_app.data.dto.UserResponse
 import com.example.danew_app.data.remote.UserApi
 import com.example.danew_app.domain.repository.UserRepository
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -23,22 +24,40 @@ class UserRepositoryImpl @Inject constructor(
     override suspend fun insertUser(user: UserEntity): UserEntity =
         suspendCancellableCoroutine { cont ->
             Log.d("User", "UserRepositoryImpl_insertUser: $user")
-            api.save(user).enqueue(object : Callback<UserEntity> {
-                override fun onResponse(call: Call<UserEntity>, response: Response<UserEntity>) {
-                    if (response.isSuccessful && response.body() != null) {
-                        // 성공: UserEntity 반환
-                        cont.resume(response.body()!!) {}
-                        Log.i("User 회원가입", "성공: ${response.body()}")
+
+            api.save(user).enqueue(object : Callback<ApiResponse<UserResponse>> {
+                override fun onResponse(
+                    call: Call<ApiResponse<UserResponse>>,
+                    response: Response<ApiResponse<UserResponse>>
+                ) {
+                    val body = response.body()
+                    if (response.isSuccessful && body != null) {
+                        if (body.status == "success" && body.data != null) {
+                            // 성공: UserResponse를 UserEntity로 변환
+                            val userEntity = UserEntity(
+                                userId = body.data.userId,
+                                createdAt = body.data.createdAt,
+                                password = user.password, // 필요한 필드 채우기
+                                name = user.name,
+                                age = user.age,
+                                gender = user.gender
+                            )
+                            cont.resume(userEntity) {}
+                            Log.i("User 회원가입", "성공: $userEntity")
+                        } else {
+                            // 서버에서 status가 error인 경우
+                            val msg = body.message ?: "회원가입 실패"
+                            cont.resumeWithException(RuntimeException(msg))
+                            Log.e("User 회원가입", "실패: $msg")
+                        }
                     } else {
-                        // 실패: 서버에서 온 메시지 포함해서 예외 던지기
                         val errorMsg = response.errorBody()?.string() ?: "회원가입 실패"
                         cont.resumeWithException(RuntimeException(errorMsg))
                         Log.e("User 회원가입", "실패: $errorMsg")
                     }
                 }
 
-                override fun onFailure(call: Call<UserEntity>, t: Throwable) {
-                    // 네트워크/통신 오류 처리
+                override fun onFailure(call: Call<ApiResponse<UserResponse>>, t: Throwable) {
                     cont.resumeWithException(t)
                     Log.e("User 회원가입", "네트워크 실패", t)
                 }
@@ -46,29 +65,34 @@ class UserRepositoryImpl @Inject constructor(
         }
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    override suspend fun login(userId: String, password: String): LoginResponse =
+    override suspend fun login(userId: String, password: String): UserResponse =
         suspendCancellableCoroutine { cont ->
-            api.login(LoginRequest(userId, password)).enqueue(object : Callback<LoginResponse> {
-                override fun onResponse(call: Call<LoginResponse>, response: Response<LoginResponse>) {
-                    if (response.isSuccessful) {
-                        val body = response.body()
-                        if (body != null && !body.token.isNullOrEmpty()) {
-                            cont.resume(body) {}
-                            Log.i("User 로그인", "성공여부: ${body.success}, 메시지: ${body.message}")
+            api.login(LoginRequest(userId, password)).enqueue(object : Callback<ApiResponse<UserResponse>> {
+                override fun onResponse(
+                    call: Call<ApiResponse<UserResponse>>,
+                    response: Response<ApiResponse<UserResponse>>) {
+                    val body = response.body()
+                    if (response.isSuccessful && body != null) {
+                        if (body.status == "success" && body.data != null) {
+                            val userResponse = body.data
+                            cont.resume(userResponse) {}
+                            Log.i("User 로그인", "성공: ${userResponse}")
                         } else {
-                            cont.resume(LoginResponse(false, "응답 바디 없음", null)) {}
-                            Log.e("User 로그인", "응답 바디 없음: ${response.code()}")
+                            // 서버에서 status가 error인 경우
+                            val msg = body.message ?: "로그인 실패"
+                            cont.resumeWithException(RuntimeException(msg))
+                            Log.e("User 로그인", "실패: $msg")
                         }
                     } else {
-                        cont.resume(LoginResponse(false, "서버 오류: ${response.code()}", null)) {}
-                        Log.e("User 로그인", "서버 오류: ${response.code()}")
+                        val errorMsg = response.errorBody()?.string() ?: "로그인 실패"
+                        cont.resumeWithException(RuntimeException(errorMsg))
+                        Log.e("User 로그인", "실패: $errorMsg")
                     }
                 }
 
-                override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
-                    cont.resume(LoginResponse(false, "네트워크 오류: ${t.localizedMessage}", null)) {}
+                override fun onFailure(call: Call<ApiResponse<UserResponse>>, t: Throwable) {
+                    cont.resumeWithException(t)
                     Log.e("User 로그인", "네트워크 실패", t)
-
                 }
             })
         }
@@ -76,46 +100,64 @@ class UserRepositoryImpl @Inject constructor(
     @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun getUser(token: String,): UserEntity =
         suspendCancellableCoroutine { cont ->
-            api.getUser(token).enqueue(object : Callback<UserEntity> {
-                override fun onResponse(call: Call<UserEntity>, response: Response<UserEntity>) {
-                    if (response.isSuccessful) {
-                        val body = response.body()
-                        if (body != null) {
-                            cont.resume(body) {}
-                            Log.i("User 조회", "${body}")
+            api.getUser(token).enqueue(object : Callback<ApiResponse<UserEntity>> {
+                override fun onResponse(call: Call<ApiResponse<UserEntity>>, response: Response<ApiResponse<UserEntity>>) {
+                    val body = response.body()
+                    if (response.isSuccessful && body != null) {
+                        if (body.status == "success" && body.data != null) {
+                            val user = body.data
+                            cont.resume(user) {}
+                            Log.i("User 조회", "성공: ${user}")
                         } else {
-                            Log.e("User 조회", "응답 바디 없음: ${response.code()}")
+                            // 서버에서 status가 error인 경우
+                            val msg = body.message ?: "유저 조회 실패"
+                            cont.resumeWithException(RuntimeException(msg))
+                            Log.e("User 조회", "실패: $msg")
                         }
                     } else {
-                        Log.e("User 조회", "서버 오류: ${response.code()}")
+                        val errorMsg = response.errorBody()?.string() ?: "로그인 실패"
+                        cont.resumeWithException(RuntimeException(errorMsg))
+                        Log.e("User 조회", "실패: $errorMsg")
                     }
                 }
 
-                override fun onFailure(call: Call<UserEntity>, t: Throwable) {
-                    Log.e("User 조회", "네트워크 실패", t)
-
+                override fun onFailure(call: Call<ApiResponse<UserEntity>>, t: Throwable) {
+                    cont.resumeWithException(t)
+                    Log.e("User 로그인", "네트워크 실패", t)
                 }
             })
         }
 
 
-    override suspend fun checkUserId(userId: String, callback: (Boolean) -> Unit) {
-        api.checkUserId(userId).enqueue(object : Callback<Boolean> {
-            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
-                if (response.body() == true) {
-                    // 사용 가능
-                    callback(true)
-                    Log.i("User 아이디 중복체크", "UserRepositoryImpl 아이디 사용 가능: ${response.body()}")
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override suspend fun checkUserId(userId: String) :Boolean =
+        suspendCancellableCoroutine{ cont ->
+        api.checkUserId(userId).enqueue(object : Callback<ApiResponse<Boolean>> {
+            override fun onResponse(call: Call<ApiResponse<Boolean>>, response: Response<ApiResponse<Boolean>>) {
+                val body = response.body()
+                Log.i("User 아이디 중복체크", "응답: ${body}")
+
+                if (response.isSuccessful && body != null) {
+                    if (body.status == "success" && body.data != null) {
+                        val isCheck = body.data
+                        cont.resume(isCheck) {}
+                        Log.i("User 아이디 중복체크", "성공: ${isCheck}")
+                    } else {
+                        // 서버에서 status가 error인 경우
+                        val msg = body.message ?: "유저 조회 실패"
+                        cont.resumeWithException(RuntimeException(msg))
+                        Log.e("User 아이디 중복체크", "실패: $msg")
+                    }
                 } else {
-                    // 중복 (409)
-                    callback(false)
-                    Log.e("User 아이디 중복체크", "UserRepositoryImpl 아이디 중복: ${response.body()}")
+                    val errorMsg = response.errorBody()?.string() ?: "아이디 중복체크 실패"
+                    cont.resumeWithException(RuntimeException(errorMsg))
+                    Log.e("User 아이디 중복체크", "실패: $errorMsg")
                 }
             }
 
-            override fun onFailure(call: Call<Boolean>, t: Throwable) {
-                // 네트워크 에러 → false 로 처리할지, 별도 분기할지는 정책에 따라 다름
-                callback(false)
+            override fun onFailure(call: Call<ApiResponse<Boolean>>, t: Throwable) {
+                cont.resumeWithException(t)
+                Log.e("User 아이디 중복체크", "네트워크 실패", t)
             }
         })
     }
