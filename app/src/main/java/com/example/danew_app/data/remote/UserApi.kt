71862d14074383285e2d70581a6c18d63e68d573
kangminju <yessuk5499@gmail.com>
2025-09-03 15:41:53 +0900

    api 반환 값 수정

--- app/src/main/java/com/example/danew_app/data/remote/UserApi.kt
+++ app/src/main/java/com/example/danew_app/data/remote/UserApi.kt
@@ -1,10 +1,9 @@
 package com.example.danew_app.data.remote
 
 import com.example.danew.data.local.entity.UserEntity
+import com.example.danew_app.data.dto.ApiResponse
 import com.example.danew_app.data.dto.LoginRequest
-import com.example.danew_app.data.dto.LoginResponse
-import com.example.danew_app.data.entity.BookmarkEntity
-import com.example.danew_app.data.entity.MetaNewsEntity
+import com.example.danew_app.data.dto.UserResponse
 
 import retrofit2.Call
 import retrofit2.http.Body
@@ -15,14 +14,14 @@ import retrofit2.http.Query
 
 interface UserApi {
     @POST("api/auth/signup")
-    fun save(@Body user: UserEntity): Call<UserEntity>
+    fun save(@Body user: UserEntity): Call<ApiResponse<UserResponse>>
 
     @POST("api/auth/login")
-    fun login(@Body loginRequest: LoginRequest): Call<LoginResponse>
+    fun login(@Body loginRequest: LoginRequest): Call<ApiResponse<UserResponse>>
 
     @GET("api/auth/check-username")
-    fun checkUserId(@Query("userId") userId: String): Call<Boolean>
+    fun checkUserId(@Query("userId") userId: String): Call<ApiResponse<Boolean>>
 
     @GET("/api/auth/getUser")
-    fun getUser(@Header("Authorization") token: String) : Call<UserEntity>
+    fun getUser(@Header("Authorization") token: String) : Call<ApiResponse<UserEntity>>
 }
