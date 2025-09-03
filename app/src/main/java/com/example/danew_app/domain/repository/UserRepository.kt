d8d9a6af22741e2db2c6ca4277105b2ce5d52f32
kangminju <yessuk5499@gmail.com>
2025-09-03 15:41:36 +0900

    함수들 반환 값 수정

--- app/src/main/java/com/example/danew_app/domain/repository/UserRepository.kt
+++ app/src/main/java/com/example/danew_app/domain/repository/UserRepository.kt
@@ -1,12 +1,12 @@
 package com.example.danew_app.domain.repository
 
 import com.example.danew.data.local.entity.UserEntity
-import com.example.danew_app.data.dto.LoginResponse
+import com.example.danew_app.data.dto.UserResponse
 
 
 interface UserRepository {
     suspend fun insertUser(user:UserEntity):UserEntity
-    suspend fun login(userId: String, password: String) : LoginResponse
-    suspend fun checkUserId(userId: String, callback: (Boolean) -> Unit)
+    suspend fun login(userId: String, password: String) : UserResponse
+    suspend fun checkUserId(userId: String):Boolean
     suspend fun getUser(token: String,): UserEntity
 }
