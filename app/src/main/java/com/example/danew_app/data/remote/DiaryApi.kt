35ef16e274748d77810c36951a0e23dce42806da
kangminju <yessuk5499@gmail.com>
2025-09-03 18:55:32 +0900

    다이어리 api 반환 수정

--- app/src/main/java/com/example/danew_app/data/remote/DiaryApi.kt
+++ app/src/main/java/com/example/danew_app/data/remote/DiaryApi.kt
@@ -1,5 +1,6 @@
 package com.example.danew_app.data.remote
 
+import com.example.danew_app.data.dto.ApiResponse
 import com.example.danew_app.data.dto.DiaryRequest
 import com.example.danew_app.data.entity.DiaryEntity
 import retrofit2.Call
@@ -14,16 +15,16 @@ import retrofit2.http.Query
 interface DiaryApi {
     @POST("api/diary/save")
     fun save(@Body diaryRequest: DiaryRequest,
-             @Header("Authorization") token:String): Call<DiaryEntity>
+             @Header("Authorization") token:String): Call<ApiResponse<DiaryEntity>>
 
     @GET("api/diary/getByDate")
     fun getByDate(@Header("Authorization") token:String,
-                  @Query("createdAt") createdAt:String): Call<DiaryEntity?>
+                  @Query("createdAt") createdAt:String): Call<ApiResponse<DiaryEntity?>>
 
     @PUT("api/diary/update/{diaryId}")
     fun updateDiary(
         @Header("Authorization") token: String,
         @Path("diaryId") diaryId: String,
         @Body diaryRequest: DiaryRequest
-    ): Call<DiaryEntity>
+    ): Call<ApiResponse<DiaryEntity>>
 }
