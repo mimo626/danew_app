3cbb354b4d93ff28736211d310792a9d12be7ade
kangminju <yessuk5499@gmail.com>
2025-09-04 13:52:03 +0900

    BookmarkMapper 수정

--- app/src/main/java/com/example/danew_app/data/mapper/BookmarkMapper.kt
+++ app/src/main/java/com/example/danew_app/data/mapper/BookmarkMapper.kt
@@ -5,7 +5,7 @@ import com.example.danew_app.domain.model.BookmarkModel
 
 fun BookmarkEntity.toDomain(): BookmarkModel {
     return BookmarkModel(
-        articleId = this.articleId,
+        articleId = this.id.articleId,
         bookmarkedAt = this.bookmarkedAt,
     )
 }
