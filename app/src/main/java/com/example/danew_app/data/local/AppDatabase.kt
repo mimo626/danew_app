715e330e1c734bab23d8ddd1e76d8748e9c9bfa0
kangminju <yessuk5499@gmail.com>
2025-09-06 18:40:38 +0900

    AppDatabase에 오늘 뉴스도 추가

--- app/src/main/java/com/example/danew_app/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/danew_app/data/local/AppDatabase.kt
@@ -2,9 +2,20 @@ package com.example.danew_app.data.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.danew_app.data.entity.SearchHistoryEntity
+import com.example.danew_app.data.entity.TodayNewsEntity
 
-@Database(entities = [SearchHistoryEntity::class], version = 1)
+@Database(
+    entities = [
+        SearchHistoryEntity::class,
+        TodayNewsEntity::class
+    ],
+    version = 2,
+    exportSchema = false
+)
+@TypeConverters(RoomConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun searchHistoryDao(): SearchHistoryDao
+    abstract fun todayNewsDao(): TodayNewsDao
 }
