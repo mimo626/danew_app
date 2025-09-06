38c952225d576976abadcb6d36f38b511e58da6d
kangminju <yessuk5499@gmail.com>
2025-09-06 19:28:43 +0900

    안 쓰는 import 제거

--- app/src/main/java/com/example/danew_app/presentation/profile/TodayNewsScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/profile/TodayNewsScreen.kt
@@ -8,8 +8,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,12 +21,7 @@ import com.example.danew_app.core.theme.ColorsLight
 import com.example.danew_app.core.widget.CustomLoadingIndicator
 import com.example.danew_app.core.widget.MainTopAppBar
 import com.example.danew_app.data.mapper.toDomain
-import com.example.danew_app.presentation.home.MainImageCard
 import com.example.danew_app.presentation.home.NewsItem
-import com.example.danew_app.presentation.home.NewsList
-import com.example.danew_app.presentation.home.NowTopNews
-import com.example.danew_app.presentation.home.SearchBar
-import com.example.danew_app.presentation.viewmodel.NewsViewModel
 import com.example.danew_app.presentation.viewmodel.TodayNewsViewModel
 
 @RequiresApi(Build.VERSION_CODES.O)
