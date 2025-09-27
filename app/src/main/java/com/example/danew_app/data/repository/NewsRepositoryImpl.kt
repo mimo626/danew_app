f6f66d797c51939acd53d3545670da0540e31742
kangminju <yessuk5499@gmail.com>
2025-09-28 01:32:25 +0900

    검색어 뉴스 조회 함수 수정

--- app/src/main/java/com/example/danew_app/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/danew_app/data/repository/NewsRepositoryImpl.kt
@@ -1,13 +1,10 @@
 package com.example.danew_app.data.repository
 
-import android.util.Log
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.example.danew_app.data.entity.NewsEntity
-import com.example.danew_app.data.mapper.toDomain
 import com.example.danew_app.data.remote.NewsApi
-import com.example.danew_app.domain.model.NewsModel
 import com.example.danew_app.domain.repository.NewsRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -17,18 +14,22 @@ import javax.inject.Singleton
 class NewsRepositoryImpl @Inject constructor(
     private val api: NewsApi
 ) : NewsRepository {
-    private var nextPage: String? = null
 
     override suspend fun getNewsByCategory(category: String): Flow<PagingData<NewsEntity>> {
         return Pager(PagingConfig(pageSize = 20)) {
-            NewsPagingSource(api, category)
+            CategoryNewsPagingSource(api, category = category)
         }.flow
     }
 
     override suspend fun getNewsBySearchQuery(searchQuery: String): Flow<PagingData<NewsEntity>> {
-        return Pager(PagingConfig(pageSize = 20)) {
-            NewsPagingSource(api, searchQuery = searchQuery)
-        }.flow
+
+        return Pager(
+            config = PagingConfig(pageSize = 10, enablePlaceholders = false),
+            pagingSourceFactory = {
+                // 검색어가 바뀔 때마다 새로운 PagingSource 인스턴스가 생성됨
+                SearchNewsPagingSource(api, searchQuery)
+            }
+        ).flow
     }
 
     override suspend fun getNewsById(id: String): List<NewsEntity> {
