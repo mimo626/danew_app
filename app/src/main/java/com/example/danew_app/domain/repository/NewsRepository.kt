5e1ce4aecf1a8b03c5cba7f356c8eb595ee8a624
kangminju <yessuk5499@gmail.com>
2025-09-27 19:07:55 +0900

    getNews 함수 선언

--- app/src/main/java/com/example/danew_app/domain/repository/NewsRepository.kt
+++ app/src/main/java/com/example/danew_app/domain/repository/NewsRepository.kt
@@ -1,9 +1,13 @@
 package com.example.danew_app.domain.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingData
 import com.example.danew_app.data.entity.NewsEntity
+import kotlinx.coroutines.flow.Flow
 
 interface NewsRepository {
     suspend fun getNewsByCategory(category: String, loadMore:Boolean): List<NewsEntity>
+    suspend fun getNews(category: String): Flow<PagingData<NewsEntity>>
     suspend fun getNewsById(id: String): List<NewsEntity>
     suspend fun getNewsBySearchQuery(searchQuery: String, loadMore:Boolean): List<NewsEntity>
 }
