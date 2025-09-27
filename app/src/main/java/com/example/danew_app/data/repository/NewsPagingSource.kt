033a2927b6f8d9e7fb630837b5492d60e595d84d
kangminju <yessuk5499@gmail.com>
2025-09-27 19:07:36 +0900

    NewsPagingSource 구현

---
+++ app/src/main/java/com/example/danew_app/data/repository/NewsPagingSource.kt
@@ -0,0 +1,43 @@
+package com.example.danew_app.data.repository
+
+import android.util.Log
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.example.danew_app.data.entity.NewsEntity
+import com.example.danew_app.data.remote.NewsApi
+
+class NewsPagingSource(
+    private val api: NewsApi,
+    private val category: String
+) : PagingSource<String, NewsEntity>() { // 키 타입을 String 으로 변경
+
+    override suspend fun load(params: LoadParams<String>): LoadResult<String, NewsEntity> {
+        return try {
+
+            val page: String? = params.key // 첫 호출은 null
+            val response = api.fetchNewsByCategory(
+                category = category,
+                page = page,
+            )
+            Log.d("NewsPagingSource", "뉴스 페이지 ${page}")
+
+
+            val newsList = response.results
+            Log.d("NewsPagingSource", "뉴스 리스트 ${newsList}")
+
+
+            LoadResult.Page(
+                data = newsList,
+                prevKey = null, // 커서 기반 API는 뒤로 가기(prev) 보통 지원 안 함
+                nextKey = response.nextPage // 서버에서 내려주는 다음 페이지 토큰
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    override fun getRefreshKey(state: PagingState<String, NewsEntity>): String? {
+        // 커서 기반일 때는 null 리턴해서 처음부터 다시 로드
+        return null
+    }
+}
