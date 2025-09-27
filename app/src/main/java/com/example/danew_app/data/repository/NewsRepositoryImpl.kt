c72771bb2b3a40d6e582e6d6860229e12536660e
kangminju <yessuk5499@gmail.com>
2025-09-27 19:07:48 +0900

    getNews 함수 구현

--- app/src/main/java/com/example/danew_app/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/danew_app/data/repository/NewsRepositoryImpl.kt
@@ -1,11 +1,15 @@
 package com.example.danew_app.data.repository
 
 import android.util.Log
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.example.danew_app.data.entity.NewsEntity
 import com.example.danew_app.data.mapper.toDomain
 import com.example.danew_app.data.remote.NewsApi
 import com.example.danew_app.domain.model.NewsModel
 import com.example.danew_app.domain.repository.NewsRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -15,6 +19,12 @@ class NewsRepositoryImpl @Inject constructor(
 ) : NewsRepository {
     private var nextPage: String? = null
 
+    override suspend fun getNews(category: String): Flow<PagingData<NewsEntity>> {
+        return Pager(PagingConfig(pageSize = 20)) {
+            NewsPagingSource(api, category)
+        }.flow
+    }
+
     override suspend fun getNewsByCategory(category: String, loadMore:Boolean): List<NewsEntity> {
         val result = api.fetchNewsByCategory(
             category = category,
@@ -22,7 +32,7 @@ class NewsRepositoryImpl @Inject constructor(
         )
 
         nextPage = result.nextPage // 다음 호출 때 사용할 page 값 저장
-        Log.d("NewsResponse", "getNewsByCategory: ${result}")
+        Log.d("News 카테고리별 조회", "getNewsByCategory: ${result}")
 
         return result.results.map { it }
     }
@@ -35,11 +45,11 @@ class NewsRepositoryImpl @Inject constructor(
     override suspend fun getNewsBySearchQuery(searchQuery: String, loadMore:Boolean): List<NewsEntity> {
         val result = api.fetchNewsBySearchQuery(
             searchQuery = searchQuery,
-            page = if (loadMore) nextPage else null
+            //page = if (loadMore) nextPage else null
         )
 
         nextPage = result.nextPage // 다음 호출 때 사용할 page 값 저장
-        Log.d("NewsResponse", "getNewsBySearchQuery: ${result}")
+        Log.d("News 검색어 조회", "getNewsBySearchQuery: ${result}")
 
         return result.results.map { it }
     }
