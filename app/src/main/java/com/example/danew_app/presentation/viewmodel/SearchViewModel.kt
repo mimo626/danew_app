e873d2b4271b930ec1755a6aef0739fb8388963a
kangminju <yessuk5499@gmail.com>
2025-09-28 22:05:28 +0900

    검색 결과 로그 추가

--- app/src/main/java/com/example/danew_app/presentation/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/example/danew_app/presentation/viewmodel/SearchViewModel.kt
@@ -1,4 +1,5 @@
 package com.example.danew_app.presentation.viewmodel
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -38,6 +39,7 @@ class SearchViewModel @Inject constructor(
             try {
                 _recentSearches.value = repository.getRecentSearches()
             } catch (e:Exception){
+                Log.e("DB 오류", "최근 검색어 조회 실패", e)
                 errorMessage = e.localizedMessage
             } finally {
                 isLoading = false
@@ -53,6 +55,7 @@ class SearchViewModel @Inject constructor(
                 repository.insertSearch(keyword)
                 loadRecentSearches()
             } catch (e:Exception){
+                Log.e("DB 오류", "최근 검색어 저장 실패", e)
                 errorMessage = e.localizedMessage
             } finally {
                 isLoading = false
@@ -69,6 +72,7 @@ class SearchViewModel @Inject constructor(
                 repository.deleteSearch(keyword)
                 loadRecentSearches()
             } catch (e:Exception){
+                Log.e("DB 오류", "최근 검색어 삭제 실패", e)
                 errorMessage = e.localizedMessage
             } finally {
                 isLoading = false
@@ -85,6 +89,7 @@ class SearchViewModel @Inject constructor(
                 repository.clearAll()
                 loadRecentSearches()
             } catch (e:Exception){
+                Log.e("DB 오류", "최근 검색어 전체 삭제 실패", e)
                 errorMessage = e.localizedMessage
             } finally {
                 isLoading = false
