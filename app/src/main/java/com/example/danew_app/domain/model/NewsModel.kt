84e8692026a44dcbbd5fc4d5ae4b3cce6d70494d
kangminju <yessuk5499@gmail.com>
2025-10-15 16:44:13 +0900

    NewsModel 수정

--- app/src/main/java/com/example/danew_app/domain/model/NewsModel.kt
+++ app/src/main/java/com/example/danew_app/domain/model/NewsModel.kt
@@ -1,14 +1,14 @@
 package com.example.danew_app.domain.model
 
 data class NewsModel(
-    val newsId: String,
-    val title: String,
-    val description: String,
-    val imageUrl: String?,
-    val sourceName: String,
-    val pubDate: String,
-    val category: List<String>?,
-    val creator: List<String>?,
-    val keywords: List<String>?,
-    val link: String?,
-    val language: String?,)
+    val newsId: String = "",
+    val title: String = "",
+    val description: String = "",
+    val imageUrl: String? = "",
+    val sourceName: String = "",
+    val pubDate: String = "",
+    val category: List<String>? = emptyList(),
+    val creator: List<String>? = emptyList(),
+    val keywords: List<String>? = emptyList(),
+    val link: String? = "",
+    val language: String? = "",)
