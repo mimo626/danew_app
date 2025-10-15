6df3e9c9b6c832ab9ecea68320068e94e9fe682b
kangminju <yessuk5499@gmail.com>
2025-10-15 16:43:22 +0900

    AIApi 주입

--- app/src/main/java/com/example/danew_app/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/danew_app/data/remote/RetrofitInstance.kt
@@ -3,8 +3,10 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 //NewsData.io api
@@ -26,6 +28,11 @@ object NewsRetrofitInstance {
 @Module
 @InstallIn(SingletonComponent::class)
 object LocalRetrofitModule {
+    val okHttpClient = OkHttpClient.Builder()
+        .connectTimeout(15, TimeUnit.SECONDS) // 연결 타임아웃
+        .readTimeout(30, TimeUnit.SECONDS)    // 응답 대기 타임아웃
+        .writeTimeout(15, TimeUnit.SECONDS)
+        .build()
 
     @Provides
     @Singleton
@@ -33,6 +40,7 @@ object LocalRetrofitModule {
         return Retrofit.Builder()
             .baseUrl("http://10.0.2.2:8080/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
             .build()
     }
 
@@ -52,6 +60,12 @@ object LocalRetrofitModule {
     @Singleton
     fun provideBookmarkApi(retrofit: Retrofit) : BookmarkApi{
         return retrofit.create(BookmarkApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideAIApi(retrofit: Retrofit) : AIApi{
+        return retrofit.create(AIApi::class.java)
     }
 }
 
