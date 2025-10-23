3d96f89d63b619455c1c6507aacc8195e03476f1
kangminju <yessuk5499@gmail.com>
2025-10-23 09:46:35 +0900

    뉴스 가져오기 함수 반환값 수정

--- app/src/main/java/com/example/danew_app/domain/usecase/GetNewsByIdUseCase.kt
+++ app/src/main/java/com/example/danew_app/domain/usecase/GetNewsByIdUseCase.kt
@@ -1,14 +1,16 @@
-package com.example.danew_app.domain.usecase
-
-import com.example.danew_app.data.mapper.toDomain
-import com.example.danew_app.domain.model.NewsModel
-import com.example.danew_app.domain.repository.NewsRepository
-import javax.inject.Inject
-
-class GetNewsByIdUseCase @Inject constructor(
-    private val repository: NewsRepository
-) {
-    suspend operator fun invoke(id: String): NewsModel{
-        return repository.getNewsById(id).toDomain()
-    }
-}
+//package com.example.danew_app.domain.usecase
+//
+//import com.example.danew_app.data.entity.NewsEntity
+//import com.example.danew_app.data.mapper.toDomain
+//import com.example.danew_app.domain.model.NewsModel
+//import com.example.danew_app.domain.repository.NewsRepository
+//import kotlinx.coroutines.flow.Flow
+//import javax.inject.Inject
+//
+//class GetNewsByIdUseCase @Inject constructor(
+//    private val repository: NewsRepository
+//) {
+//    suspend operator fun invoke(id: String): Flow<NewsModel> {
+//        return repository.getNewsById(id).toDomain()
+//    }
+//}
