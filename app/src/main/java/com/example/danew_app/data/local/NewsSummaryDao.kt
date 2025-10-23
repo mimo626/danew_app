6a3afd64abbeb2b2c834058a9091cfdf9fbf6ea5
kangminju <yessuk5499@gmail.com>
2025-10-23 09:44:43 +0900

    NewsSummaryDao 구현

---
+++ app/src/main/java/com/example/danew_app/data/local/NewsSummaryDao.kt
@@ -0,0 +1,17 @@
+package com.example.danew_app.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.danew_app.data.entity.NewsSummaryEntity
+
+@Dao
+interface NewsSummaryDao {
+
+    @Query("SELECT * FROM news_summary WHERE articleId = :id LIMIT 1")
+    suspend fun getSummarizedNews(id: String): NewsSummaryEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNewsSummary(summary: NewsSummaryEntity)
+}
