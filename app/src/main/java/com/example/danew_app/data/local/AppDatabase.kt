fc32bf0ede3d2b10eac941dafc0791d7503d942d
kangminju <yessuk5499@gmail.com>
2025-10-23 09:44:22 +0900

    뉴스 dao 로컬 db에 추가

--- app/src/main/java/com/example/danew_app/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/danew_app/data/local/AppDatabase.kt
@@ -3,19 +3,22 @@ package com.example.danew_app.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.danew_app.data.entity.NewsSummaryEntity
 import com.example.danew_app.data.entity.SearchHistoryEntity
 import com.example.danew_app.data.entity.TodayNewsEntity
 
 @Database(
     entities = [
         SearchHistoryEntity::class,
-        TodayNewsEntity::class
+        TodayNewsEntity::class,
+        NewsSummaryEntity::class,
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 @TypeConverters(RoomConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun searchHistoryDao(): SearchHistoryDao
     abstract fun todayNewsDao(): TodayNewsDao
+    abstract fun newsSummaryDao(): NewsSummaryDao
 }
