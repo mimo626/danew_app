92fe44360d4471bdbb8c14b15f40abbdf0157cf8
kangminju <yessuk5499@gmail.com>
2025-10-31 18:28:02 +0900

    import문 제거

--- app/src/main/java/com/example/danew_app/presentation/home/NowTopNews.kt
+++ app/src/main/java/com/example/danew_app/presentation/home/NowTopNews.kt
@@ -1,26 +1,18 @@
 package com.example.danew_app.presentation.home
 
-import android.util.Log
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavHostController
 import com.example.danew_app.domain.model.NewsModel
 
 // 현재 Top 뉴스 위젯
