c6e779cbac897f6eb0d0ed37e0e01c339b6afb65
kangminju <yessuk5499@gmail.com>
2025-11-12 14:17:08 +0900

    안 쓰는 import 구현

--- app/src/main/java/com/example/danew_app/presentation/profile/KeywordUpdateScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/profile/KeywordUpdateScreen.kt
@@ -15,14 +15,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
