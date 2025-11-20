62d2422e7b969673fd8366ba34e50dd54458eadd
kangminju <yessuk5499@gmail.com>
2025-11-20 14:45:03 +0900

    getBookmarkNews 선언

--- app/src/main/java/com/example/danew_app/domain/repository/BookmarkRepository.kt
+++ app/src/main/java/com/example/danew_app/domain/repository/BookmarkRepository.kt
@@ -6,6 +6,7 @@ import com.example.danew_app.data.entity.MetaNewsEntity
 interface BookmarkRepository {
     suspend fun saveBookmark(token:String, metaNewsEntity: MetaNewsEntity):BookmarkEntity
     suspend fun getBookmarks(token: String):List<MetaNewsEntity>
+    suspend fun getBookmarkNews(newsId: String): MetaNewsEntity
     suspend fun checkBookmark(token:String, newsId:String):Boolean
     suspend fun deleteBookmark(token:String, newsId:String):Boolean
 }
