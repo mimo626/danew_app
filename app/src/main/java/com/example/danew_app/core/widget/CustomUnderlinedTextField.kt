e575fdca3232bbfa289d873656fd73ba43c9af8e
kangminju <yessuk5499@gmail.com>
2025-11-21 21:40:35 +0900

    안 쓰는 import 제거

--- app/src/main/java/com/example/danew_app/core/widget/CustomUnderlinedTextField.kt
+++ app/src/main/java/com/example/danew_app/core/widget/CustomUnderlinedTextField.kt
@@ -2,8 +2,6 @@ package com.example.danew_app.core.widget
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Face
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
