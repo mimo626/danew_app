c993b9b5c9ecfa75fd5da3c4813429f76c3bd6cd
kangminju <yessuk5499@gmail.com>
2025-11-21 21:08:06 +0900

    안 쓰는 import 제거

--- app/src/main/java/com/example/danew_app/presentation/profile/KeywordUpdateScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/profile/KeywordUpdateScreen.kt
@@ -1,7 +1,6 @@
 package com.example.danew_app.presentation.profile
 
 import android.annotation.SuppressLint
-import android.widget.Toast // Toast 임포트 필요
 import com.example.danew_app.presentation.viewmodel.UserViewModel
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -23,7 +22,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -39,7 +37,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext // Context 임포트 필요
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
