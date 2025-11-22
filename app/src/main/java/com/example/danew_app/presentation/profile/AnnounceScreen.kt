c4a6118f83e4995d6c161b3f2d4204db891e4323
kangminju <yessuk5499@gmail.com>
2025-11-22 16:33:55 +0900

    AnnounceScreen 생성

---
+++ app/src/main/java/com/example/danew_app/presentation/profile/AnnounceScreen.kt
@@ -0,0 +1,69 @@
+package com.example.danew.presentation.profile
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import com.example.danew_app.core.theme.ColorsLight
+import com.example.danew_app.core.widget.MainTopAppBar
+
+@Composable
+fun AnnounceScreen(navController: NavHostController) {
+
+    Scaffold(
+        containerColor = ColorsLight.whiteColor,
+        topBar = {
+            MainTopAppBar(
+                navController = navController,
+                title = "공지사항",
+                icon = Icons.Default.Settings,
+                isHome = false)
+            {
+                navController.navigate("settings")
+            }
+        },
+        bottomBar = {
+        }
+    ) { padding ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            when {
+//                isLoading -> {
+//                    item {
+//                        LazyLoadingIndicator(padding)
+//                    }
+//                }
+
+//                announceList.isNotEmpty() -> {
+//                    item { Spacer(modifier = Modifier.height(16.dp)) }
+//                    items(announceList) {
+//                        MyPageMenuItem("${announceList.title}") { }
+//                    }
+//                    item { Spacer(modifier = Modifier.height(28.dp)) }
+//                }
+
+                else -> {
+                    item {
+                        Box(
+                            modifier = Modifier
+                                .fillParentMaxSize()
+                                .padding(padding),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            Text("공지사항이 없습니다")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
