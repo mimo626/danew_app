48ee875d71316938509cdec756d0275abe3faf61
kangminju <yessuk5499@gmail.com>
2025-11-24 00:30:34 +0900

    addConverterFactory 추가

--- app/src/main/java/com/example/danew_app/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/example/danew_app/data/remote/RetrofitInstance.kt
@@ -6,6 +6,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -39,6 +40,7 @@ object LocalRetrofitModule {
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl("http://10.0.2.2:8080/")
+            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .client(okHttpClient)
             .build()
