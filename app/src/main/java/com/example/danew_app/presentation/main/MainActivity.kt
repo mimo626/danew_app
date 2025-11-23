ecd00c39f55343dc2e6225fb0c951ddfe5dc5f26
kangminju <yessuk5499@gmail.com>
2025-11-24 00:30:22 +0900

    MainActivity에 firebase 설정 추가

--- app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.danew_app.presentation.main
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
 import androidx.navigation.compose.rememberNavController
 import com.example.danew.presentation.main.MainScreen
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,6 +22,18 @@ class MainActivity : ComponentActivity() {
 
         val isLoggedIn = intent.getBooleanExtra("isLoggedIn", false)
 
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                Log.w("FCM", "토큰 가져오기 실패", task.exception)
+                return@addOnCompleteListener
+            }
+
+            // 현재 단말기의 FCM 토큰 가져옴
+            val token = task.result
+            Log.d("FCM", "Current Token: $token")
+
+            // 이 토큰을 복사해서 Postman이나 서버 API 테스트에 사용하세요.
+        }
         setContent {
             val navHostController = rememberNavController()
 
