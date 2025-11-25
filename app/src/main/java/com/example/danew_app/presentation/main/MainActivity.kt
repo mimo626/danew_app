049c29126e02a5a0e8c5b5b0d89658b0e6bb974a
kangminju <yessuk5499@gmail.com>
2025-11-25 23:59:42 +0900

    안 쓰는 Import 제거

--- app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/danew_app/presentation/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.danew_app.presentation.main
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
