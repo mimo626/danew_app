acfb358f7a928f376a32d2a4b12e7656ad6bc30f
kangminju <yessuk5499@gmail.com>
2025-11-25 22:45:54 +0900

    알림 api 구현

---
+++ app/src/main/java/com/example/danew_app/data/remote/NotificationApi.kt
@@ -0,0 +1,14 @@
+package com.example.danew_app.data.remote
+
+import retrofit2.Call
+import retrofit2.http.Header
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface NotificationApi {
+    @POST("/api/notifications/send")
+    fun sendSelfNotification(
+        @Header("X-FCM-Token") token: String,
+        @Query("nickname") nickname: String
+    ): Call<String>
+}
