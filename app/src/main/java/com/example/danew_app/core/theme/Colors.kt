d4e45827afb877b0a01be162f3fab3d544757a3f
kangminju <yessuk5499@gmail.com>
2025-12-28 01:59:49 +0900

    색상 파일 수정 및 통합

--- app/src/main/java/com/example/danew_app/core/theme/Colors.kt
+++ app/src/main/java/com/example/danew_app/core/theme/Colors.kt
@@ -2,7 +2,7 @@ package com.example.danew_app.core.theme
 
 import androidx.compose.ui.graphics.Color
 
-object ColorsLight {
+object DanewColors {
     val primaryColor = Color(0xFF0B942D)
     val secondaryColor = Color(0xFFDFF0E3)
     val lightGrayColor = Color(0xFFF0F0F0)
@@ -12,16 +12,4 @@ object ColorsLight {
     val blueColor = Color(0xFF3182F7)
     val blackColor = Color(0xFF000000);
     val whiteColor = Color(0xFFFFFFFF);
-}
-
-object ColorsDark {
-    val primaryColor = Color(0xFF0B942D)
-    val secondaryColor = Color(0xFF9BA18D)
-    val lightGrayColor = Color(0xFF2A2A2A)
-    val darkGrayColor = Color(0xFF121212)
-    val grayColor = Color(0xFF444444)
-    val redColor = Color(0xFFFF6F71)
-    val blueColor = Color(0xFF90C9F9)
-    val blackColor = Color(0xFF000000)
-    val whiteColor = Color(0xFFFFFFFF)
 }
