80aaff5f8b07d3459731a76e3921475e8a346517
kangminju <yessuk5499@gmail.com>
2025-12-28 17:19:48 +0900

    로딩 테마 수정

--- app/src/main/java/com/example/danew_app/core/widget/LoadingIndicator.kt
+++ app/src/main/java/com/example/danew_app/core/widget/LoadingIndicator.kt
@@ -3,11 +3,11 @@ package com.example.danew_app.core.widget
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.danew_app.core.theme.ColorsLight
 
 @Composable
 fun LazyItemScope.LazyLoadingIndicator(
@@ -19,7 +19,7 @@ fun LazyItemScope.LazyLoadingIndicator(
             .padding(padding),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator(color = ColorsLight.grayColor)
+        CircularProgressIndicator(color = MaterialTheme.colorScheme.secondary)
     }
 }
 
@@ -33,7 +33,8 @@ fun CustomLoadingIndicator(
             .padding(padding),
         contentAlignment = Alignment.Center
     ) {
-        CircularProgressIndicator(color = ColorsLight.grayColor)
+        CircularProgressIndicator(color = MaterialTheme.colorScheme.secondary
+        )
     }
 }
 
