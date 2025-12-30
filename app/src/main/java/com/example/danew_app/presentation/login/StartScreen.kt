062491aaf6d9415a809564585ebd88e725d008f4
kangminju <yessuk5499@gmail.com>
2025-12-31 01:45:54 +0900

    시작 화면 테마 수정

--- app/src/main/java/com/example/danew_app/presentation/login/StartScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/login/StartScreen.kt
@@ -3,6 +3,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,21 +13,21 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.danew_app.core.theme.ColorsLight
+import com.example.danew_app.core.theme.DanewColors
 import com.example.danew_app.core.widget.BottomButton
 
 @Composable
 fun StartScreen(navHostController: NavHostController,) {
 
     Scaffold(
-        containerColor = ColorsLight.whiteColor,
+        containerColor = MaterialTheme.colorScheme.background,
         bottomBar = {
             Column {
                 BottomButton(text = "로그인") {
                     navHostController.navigate("login")
                 }
                 BottomButton(text = "회원가입",
-                    backgroundColor = ColorsLight.grayColor) {
+                    backgroundColor = MaterialTheme.colorScheme.onSurface) {
                     navHostController.navigate("signup")
                 }
             }
@@ -42,7 +43,8 @@ fun StartScreen(navHostController: NavHostController,) {
         {
             Text("DANEW", style = TextStyle(fontSize = 40.sp,
                 fontWeight = FontWeight.Bold,
-                color = ColorsLight.primaryColor,),
+                color = MaterialTheme.colorScheme.primary,
+                ),
                 modifier = Modifier.align(Alignment.CenterHorizontally) // Column 안에서는 가로 정렬
             )
         }
