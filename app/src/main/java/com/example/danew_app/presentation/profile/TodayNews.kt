9ed8b76e44e99e4980e3d7193ef2be29d28c9787
kangminju <yessuk5499@gmail.com>
2025-12-31 01:46:00 +0900

    오늘의 뉴스 테마 수정

--- app/src/main/java/com/example/danew_app/presentation/profile/TodayNews.kt
+++ app/src/main/java/com/example/danew_app/presentation/profile/TodayNews.kt
@@ -22,7 +22,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
