f07e9bf613df9785017fa11b0511ed3456dc9f11
kangminju <yessuk5499@gmail.com>
2025-12-30 23:41:28 +0900

    CustomLinearProgressIndicator 테마 수정

--- app/src/main/java/com/example/danew_app/core/widget/CustomLinearProgressIndicator.kt
+++ app/src/main/java/com/example/danew_app/core/widget/CustomLinearProgressIndicator.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -18,6 +19,6 @@ fun CustomLinearProgressIndicator (progress:Float){
             .fillMaxWidth()
             .height(6.dp)
             .clip(RoundedCornerShape(4.dp)),
-        color = DanewColors.primaryColor,
-        trackColor = DanewColors.lightGrayColor,)
+        color = MaterialTheme.colorScheme.primary,
+        trackColor = MaterialTheme.colorScheme.background,)
 }
