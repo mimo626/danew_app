9f5d72d83b57acbc0a570dbbf5ee1814e31eaaf5
kangminju <yessuk5499@gmail.com>
2025-12-30 23:43:40 +0900

    설정 화면 테마 수정

--- app/src/main/java/com/example/danew_app/presentation/profile/SettingsScreen.kt
+++ app/src/main/java/com/example/danew_app/presentation/profile/SettingsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,7 +16,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
-import com.example.danew_app.core.theme.ColorsLight
+import com.example.danew_app.core.theme.DanewColors
 import com.example.danew_app.core.widget.MainTopAppBar
 
 @Composable
@@ -25,7 +26,7 @@ fun SettingScreen(navHostController: NavHostController,) {
         "알림 설정", "개인정보 처리 방침", "서비스 이용약관", "탈퇴하기"
     )
     Scaffold(
-        containerColor = ColorsLight.whiteColor,
+        containerColor = MaterialTheme.colorScheme.background,
         topBar = {
             MainTopAppBar( navController = navHostController,
                 title = "설정",
@@ -48,10 +49,11 @@ fun SettingScreen(navHostController: NavHostController,) {
                         setting,
                         fontWeight = FontWeight.Medium,
                         fontSize = 14.sp,
-                        color = if(setting != "탈퇴하기") ColorsLight.blackColor else ColorsLight.grayColor,
+                        color = if(setting != "탈퇴하기") MaterialTheme.colorScheme.onSecondary else MaterialTheme.colorScheme.onSurface,
                         modifier = Modifier.padding(horizontal = 20.dp).padding(vertical = 16.dp)
                     )
-                    HorizontalDivider(color = ColorsLight.lightGrayColor)
+                    HorizontalDivider(color = MaterialTheme.colorScheme.secondary,
+                    )
                 }
             }
         }
